ab5052246a3ee2c093c9b27561bf1271a6c79938
ranfei <ran.fei09@gmail.com>
2015-03-31 21:09:10 +0100

    Update Customer.java

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -2,77 +2,119 @@ package com.abc;
 
 import java.util.ArrayList;
 import java.util.List;
+/**
+ *
+ * @author R. Fei
+ */
+class Customer {
+    private final String name;
+    private final List<Account> accounts;
+    private PrintStatements Printer;
 
-import static java.lang.Math.abs;
-
-public class Customer {
-    private String name;
-    private List<Account> accounts;
-
-    public Customer(String name) {
+    Customer(String name) {
         this.name = name;
-        this.accounts = new ArrayList<Account>();
+        this.accounts = new ArrayList<>(); 
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public Customer openAccount(Account account) {
+    void openAccount(Account account) {
         accounts.add(account);
-        return this;
     }
 
-    public int getNumberOfAccounts() {
+    int getNumberOfAccounts() {
         return accounts.size();
     }
-
-    public double totalInterestEarned() {
+    /* Transfer money between different accounts of the customer
+     * No overdraft is allowed
+     */
+    void transferBetweenAccounts(int indexFrom, int indexTo, double amount){
+        if (amount <= 0){
+            throw new IllegalArgumentException
+                                          ("amount must be greater than zero!");
+        }
+        if (indexTo == indexFrom){
+            throw new IllegalArgumentException
+                ("Please select another account to transfer your money!");
+        } 
+        try{
+            if (accounts.get(indexFrom).getBalance() < amount) {
+                throw new IllegalArgumentException
+                                 ("There is not enough money in this account!");
+            }
+            accounts.get(indexFrom).TransferMoney(amount, accounts.get(indexTo));
+        }
+        catch(Exception e){
+            System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+        }
+    }
+    /* Total interest accured from all bank accounts of this customer */
+    double totalInterestEarned() {
         double total = 0;
-        for (Account a : accounts)
-            total += a.interestEarned();
+        total = accounts.stream().map((a) ->
+                a.interestEarned()).reduce(total, (accumulator, _item) ->
+                    accumulator + _item);
         return total;
     }
-
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
+    /* Required by Printing Statements */
+    List<Account> getAccounts(){
+        return accounts;
     }
 
-    private String statementForAccount(Account a) {
-        String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
-
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
-        return s;
+    public String Print() {
+        this.Printer = new PrintStatements(accounts, name);
+        if ( null != Printer )
+            return Printer.PrintOut();
+        else
+            return "Bad Printer Error";
     }
+    /***************************************************************************
+     * Nested class for Print() method.
+     **************************************************************************/
+    private class PrintStatements implements Printer{
+    
+        private final List<Account> accounts;
+        private final String name;
+        private final Formatter format;
 
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
-    }
-}
+        private PrintStatements(List<Account> accountList, String name){
+            this.accounts = accountList;
+            this.name = name;
+            this.format = new Formatter();
+        }
+        @Override
+        public String PrintOut(){
+            return this.formatCustomer();
+        }
+        /* Print the statement of all bank accounts under this customer:
+         * " Statement for Henry
+         *   (blank line)                    
+         *   Checking Account
+         *     deposit $100.00\n"+
+         *   Total $100.00\n"+
+         *   (2 blank lines)
+         *   Savings Account\n"+
+         *     deposit $1,000.00
+         *   Total $1,000.00\n"+
+         *   (1 blank line)
+         *   Total In All Accounts $1,100.00  "
+         */
+        private String formatCustomer(){
+            if( null != accounts && !accounts.isEmpty() ){
+                String s = "Statement for " + name + "\n";
+                double total = 0.0;
+                for (Account a : accounts) {
+                    double balance = a.getBalance();
+                    s += "\n" + a.Print() + "\n";
+                    total += balance;
+                }
+                s += "Total In All Accounts " + format.toDollars(total);
+                return s;
+            }
+            else
+                return "No Account is Related to " + name;
+        }
+    }//End of nested PrintStatements class 
+}//End of Customer class
