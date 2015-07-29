a52ffeb1f8eb661745ce626c70a954ed124bf322
chickenpie24 <wesley.hewitt@hotmail.co.uk>
2015-07-29 17:15:05 +0100

    Update DateProvider.java
    Removed unnecessary function getInstance(), added function then(int days) - returns a date for x days in the future, added function tillMidnight() - returns time until midnight

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -4,15 +4,27 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateProvider {
-    private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
-
-    public Date now() {
+    public static Date now() {
         return Calendar.getInstance().getTime();
+    }
+    
+    // Returns a Date for x days in the future
+    public static Date then(int days){
+    	Calendar c = Calendar.getInstance();
+    	c.add(Calendar.DATE, days);
+    	return c.getTime();
+    }
+    
+    // Returns the time till midnight
+    // Used for interest, can be changed to whatever time the bank wishes to apply interest
+    public static long tillMidnight(){
+	    Calendar c = Calendar.getInstance();
+	    c.add(Calendar.DAY_OF_MONTH, 1);
+	    c.set(Calendar.HOUR_OF_DAY, 0);
+	    c.set(Calendar.MINUTE, 0);
+	    c.set(Calendar.SECOND, 0);
+	    c.set(Calendar.MILLISECOND, 0);
+	    return (c.getTimeInMillis()-System.currentTimeMillis());
     }
 }
