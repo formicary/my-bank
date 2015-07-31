e82fbe47e8999693b6eaed430aa2cc9cde985539
spspspgithub <sorb.patel@outlook.com>
2015-07-31 12:16:49 +0100

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,15 @@
-package com.abc;
+package test.java.com.abc;
+
+import main.java.com.abc.Transaction;
 
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
-    @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
-    }
+	@Test
+	public void transaction() {
+		Transaction t = new Transaction(5);
+		assertTrue(t instanceof Transaction);
+	}
 }
