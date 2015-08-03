3039d16ae026ab74cd0422e0be6d0228cfc9c530
Jonathan Tuckwell <jtuckwell@gmail.com>
2015-08-03 08:37:23 +0100

    Improve Transaction tests

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+
 public class TransactionTest {
 	private static final double DOUBLE_DELTA = 1e-15;
 
@@ -13,5 +15,13 @@ public class TransactionTest {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
         assertEquals(t.amount, 5, DOUBLE_DELTA);
+    }
+
+    @Test
+    public void transaction_date() {
+        Transaction t = new Transaction(5, new Date(0));
+        assertTrue(t instanceof Transaction);
+        assertEquals(t.amount, 5, DOUBLE_DELTA);
+        assertTrue(t.getTransactionDate().compareTo(new Date(0)) == 0);
     }
 }
