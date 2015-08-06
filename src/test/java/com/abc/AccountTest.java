b40b96206a1a978981996d914aaef04822cbd52f
igalna <igalna@gmail.com>
2015-08-06 15:24:40 +0100

    Account Testing

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -7,20 +7,47 @@ import org.junit.Test;
 
 public class AccountTest {
 
-	private final static double VERY_SMALL_AMOUNT = 1e-15;
+	private final static double DELTA = 1e-15;
 	
 	@Test
 	public void testInterestEarnedChecking() {
 		Account checking = new Account(Account.CHECKING);
 		checking.deposit(100);
-		assertEquals(0.1, checking.interestEarned(), VERY_SMALL_AMOUNT);
+		assertEquals(0.1, checking.interestEarned(), DELTA);
 	}
 	
 	@Test
 	public void testInterestEarnedSavingsLessThan1000() {
 		Account savings = new Account(Account.SAVINGS);
 		savings.deposit(999);
-		assertEquals(0.999, savings.interestEarned(), VERY_SMALL_AMOUNT);
+		assertEquals(0.999, savings.interestEarned(), DELTA);
 	}
 
+	@Test
+	public void testInterestEarnedSavingsGreaterThan1000() {
+		Account savings = new Account(Account.SAVINGS);
+		savings.deposit(2100);
+		assertEquals(3.2, savings.interestEarned(), DELTA);
+	}
+	
+	@Test
+	public void testInterestEarnedMaxiTwoPercent() {
+		Account maxi = new Account(Account.MAXI_SAVINGS);
+		maxi.deposit(1000);
+		assertEquals(20.0, maxi.interestEarned(), DELTA);
+	}
+	
+	@Test
+	public void testInterestEarnedMaxiFivePercent() {
+		Account maxi = new Account(Account.MAXI_SAVINGS);
+		maxi.deposit(1500);
+		assertEquals(45.0,  maxi.interestEarned(), DELTA);
+	}
+	
+	@Test
+	public void testInterestEarnedMaxiTenPercent() {
+		Account maxi = new Account(Account.MAXI_SAVINGS);
+		maxi.deposit(3000);
+		assertEquals(170.0, maxi.interestEarned(), DELTA);
+	}
 }
