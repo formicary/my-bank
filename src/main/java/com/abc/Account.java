270858b860d6eabdb1ce43b27519ac7225558838
benrebak <ben.rebak@outlook.com>
2015-08-11 00:54:05 +0100

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,73 +1,104 @@
 package com.abc;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Account {
+public abstract class Account {
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
+	public static final int CHECKING = 0;
+	public static final int MAXI_SAVINGS = 1;
+	public static final int SAVINGS = 2;
 
-    private final int accountType;
-    public List<Transaction> transactions;
+	protected final int accountNumber;
+	protected final int accountType;
 
-    public Account(int accountType) {
-        this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
-    }
+	/*
+	 * balance variable reduces the overhead of continually recalculating
+	 * transaction sums. BigDecimal types are used to store currency values, as
+	 * they permit more accurate arithmetic operations than floating point /
+	 * double representations.
+	 */
 
-    public void deposit(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("amount must be greater than zero");
-        } else {
-            transactions.add(new Transaction(amount));
-        }
-    }
+	protected BigDecimal balance = BigDecimal.ZERO;
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+	protected List<Transaction> transactions;
 
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
-        }
-    }
+	public Account(int accountType, int accountNumber) {
+		this.accountType = accountType;
+		/*
+		 * allows the customer to transfer to/from accounts previously opened.
+		 * Using the accountnumber identifier, each of a customer's accounts is
+		 * uniquely identifiable from the others.
+		 */
+		this.accountNumber = accountNumber;
+		this.transactions = new ArrayList<Transaction>();
+	}
 
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
+	/*
+	 * Alters the input representation to BigDecimal, and adds it to the list of
+	 * transactions for the account.
+	 */
+	public void deposit(double input) {
+		BigDecimal amount = new BigDecimal(String.valueOf(input));
 
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
-        return amount;
-    }
+		try {
+			if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+				throw new IllegalArgumentException(
+						"Amount must be greater than zero.");
+			}
+		} catch (IllegalArgumentException iae) {
+			System.out.println("Error - " + iae.getMessage());
+			return;
+		}
 
-    public int getAccountType() {
-        return accountType;
-    }
+		balance = balance.add(amount);
+		transactions.add(new Transaction(amount));
 
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public int getAccountType() {
+		return accountType;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	abstract BigDecimal getInterestRateForBalance();
+
+	abstract BigDecimal interestEarned();
+
+	/*
+	 * As in deposit, but with a conditional statement to see whether the
+	 * account contains enough money to complete the request
+	 */
+	public boolean withdraw(double input) {
+		BigDecimal amount = new BigDecimal(String.valueOf(input));
+
+		try {
+			if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+				throw new IllegalArgumentException(
+						"Amount must be greater than zero.");
+			}
+		} catch (IllegalArgumentException iae) {
+			System.out.println("Error - " + iae.getMessage());
+			return false;
+		}
+
+		if (balance.compareTo(amount) < 0) {
+			System.out
+					.println("Unable to complete action: insufficient funds.\n"
+							+ "You currently have $" + balance + " available.");
+			return false;
+		} else {
+			balance = balance.subtract(amount);
+			transactions.add(new Transaction(BigDecimal.ZERO.subtract(amount)));
+			return true;
+		}
+	}
 }
