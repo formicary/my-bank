00456398409bb7a647cc445f7a25ed2ee9304a81
benrebak <ben.rebak@outlook.com>
2015-08-11 00:57:04 +0100

    Update DateProvider.java

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -4,15 +4,28 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateProvider {
-    private static DateProvider instance = null;
+	/* ten days expressed in milliseconds - for use in madeRecentWithdrawal */
+	private static long oneDay = 1000 * 60 * 60 * 24;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	/**
+	 * @return the current time
+	 */
+	public static Date now() {
+		return Calendar.getInstance().getTime();
+	}
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	/**
+	 * 
+	 * @param d
+	 *            a Date object
+	 * @param num
+	 *            the number of full days between the desired Date and the input
+	 *            Date
+	 * @return the Date object that was exactly num days before d
+	 */
+	public static Date daysBefore(Date d, int num) {
+		long time = num * oneDay;
+		Date period = new Date(d.getTime() - time);
+		return period;
+	}
 }
