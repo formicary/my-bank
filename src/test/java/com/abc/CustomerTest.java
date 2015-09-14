40167f1c100ef1070e90e2e8540bb7137c4649b9
samal231 <sammarm@hotmail.com>
2015-09-14 14:44:21 +0100

    Update CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -1,5 +1,10 @@
 package com.abc;
 
+import main.java.com.abc.Account;
+import main.java.com.abc.CheckingsAccount;
+import main.java.com.abc.Customer;
+import main.java.com.abc.SavingsAccount;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -10,8 +15,8 @@ public class CustomerTest {
     @Test //Test customer statement generation
     public void testApp(){
 
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new CheckingsAccount();
+        Account savingsAccount = new SavingsAccount();
 
         Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
 
@@ -21,13 +26,13 @@ public class CustomerTest {
 
         assertEquals("Statement for Henry\n" +
                 "\n" +
-                "Checking Account\n" +
+                "Checkings Account" +
                 "  deposit $100.00\n" +
                 "Total $100.00\n" +
                 "\n" +
-                "Savings Account\n" +
+                "Savings Account" +
                 "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
+                "  withdrawal $-200.00\n" +
                 "Total $3,800.00\n" +
                 "\n" +
                 "Total In All Accounts $3,900.00", henry.getStatement());
@@ -35,23 +40,39 @@ public class CustomerTest {
 
     @Test
     public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
+        Customer oscar = new Customer("Oscar").openAccount(new SavingsAccount());
         assertEquals(1, oscar.getNumberOfAccounts());
     }
 
     @Test
     public void testTwoAccount(){
         Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+                .openAccount(new SavingsAccount());
+        oscar.openAccount(new CheckingsAccount());
         assertEquals(2, oscar.getNumberOfAccounts());
     }
 
     @Ignore
     public void testThreeAcounts() {
         Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+                .openAccount(new SavingsAccount());
+        oscar.openAccount(new CheckingsAccount());
         assertEquals(3, oscar.getNumberOfAccounts());
+    }
+    
+    /**
+     * Testing if money transferring accounts works.
+     */
+    @Test
+    public void testTransferAccount(){
+    	Account sAccount = new SavingsAccount();
+    	Account cAccount = new CheckingsAccount();
+    	sAccount.deposit(100);
+        Customer oscar = new Customer("Oscar");
+        
+        oscar.openAccount(sAccount);
+        oscar.openAccount(cAccount);
+        oscar.transferAccount(sAccount, cAccount, 100);
+        assertEquals(100, cAccount.getBalance(), 0.001);
     }
 }
