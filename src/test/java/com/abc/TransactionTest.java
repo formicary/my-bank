baa39e56f0f0eca8b1fdb3500db4ec834988db13
zknill <zrjknill@gmail.com>
2015-10-14 20:25:21 +0100

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,14 @@
 package com.abc;
 
 import org.junit.Test;
+import java.math.BigDecimal;
 
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
     @Test
     public void transaction() {
-        Transaction t = new Transaction(5);
+        Transaction t = new Transaction(new BigDecimal("5"));
         assertTrue(t instanceof Transaction);
     }
 }
