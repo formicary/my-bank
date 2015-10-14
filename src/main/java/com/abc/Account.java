c1777e7e4667dbc5d234befa2b00420456ff096f
zknill <zrjknill@gmail.com>
2015-10-14 20:21:48 +0100

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -2,6 +2,9 @@ package com.abc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Account {
 
@@ -21,48 +24,96 @@ public class Account {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
         } else {
-            transactions.add(new Transaction(amount));
+            transactions.add(new Transaction(BigDecimal.valueOf(amount)));
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
-
-    public double interestEarned() {
-        double amount = sumTransactions();
+	public void withdraw(double amount) {
+	    if (amount <= 0) {
+	        throw new IllegalArgumentException("amount must be greater than zero");
+	    } else {
+	        transactions.add(new Transaction(BigDecimal.valueOf(-amount)));
+	    }
+	}
+	
+	
+    public BigDecimal interestEarned() {
+        BigDecimal amount = sumTransactions();
+        BigDecimal result;
+        BigDecimal a;
+        MathContext mc = new MathContext(4);
         switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
+        	case CHECKING:
+        		a = new BigDecimal(new BigDecimal("0.001").divide(new BigDecimal("365"), mc).toString());
+        		a = a.add(new BigDecimal("1"));
+        		a = a.pow(365, mc);
+        		a = a.multiply(amount, mc);
+        		return a.subtract(amount, mc);
+        	case SAVINGS:
+            	if(amount.compareTo(new BigDecimal("1000")) <= 0){
+            		a = new BigDecimal(new BigDecimal("0.001").divide(new BigDecimal("365"), mc).toString());
+            		a = a.add(new BigDecimal("1"));
+            		a = a.pow(365, mc);
+            		a = a.multiply(amount, mc);
+            		return a.subtract(amount, mc);
+            	} else {
+            		a = new BigDecimal(new BigDecimal("0.002").divide(new BigDecimal("365"), mc).toString());
+            		a = a.add(new BigDecimal("1"));
+            		a = a.pow(365, mc);
+            		a = a.multiply(amount, mc);
+            		a = a.add(new BigDecimal("1"));
+                	return a.subtract(amount, mc);
+            	}
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
+            	//5% if no withdrawals in last 10days, else 0.1%
+            	BigDecimal b;
+            	Transaction lastTransaction = transactions.get(transactions.size()-1);
+            	Date dateOne = new Date();
+            	Date dateTwo = lastTransaction.transactionDate;
+            	long dateDiff = dateOne.getTime() - dateTwo.getTime();
+            	long dayDiff = dateDiff/(24 * 60 * 60 * 1000);
+            	if(dayDiff > 10){ 
+            		b = new BigDecimal(new BigDecimal("0.05").divide(new BigDecimal("365"), mc).toString());
+					b = b.add(new BigDecimal("1"));
+            		b = b.pow(365, mc);
+            		b = b.multiply(amount, mc);
+            		result = amount.multiply(new BigDecimal("0.05"), mc);
+            		return b.subtract(amount, mc);
+            	} else {
+            		boolean withdrawal = false;
+            		for(int i = 1; i < transactions.size(); i++){
+            			lastTransaction = transactions.get(transactions.size()-i);
+            			dateTwo = lastTransaction.transactionDate;
+            			dateDiff = dateOne.getTime() - dateTwo.getTime();
+            			dayDiff = dayDiff = dateDiff/(24 * 60 * 60 * 1000);
+            			if(dayDiff < 10){
+            				if(lastTransaction.amount.compareTo(BigDecimal.ZERO) < 0){
+            					withdrawal = true;
+            					a = new BigDecimal(new BigDecimal("0.001").divide(new BigDecimal("10"), mc).toString());
+                        		a = a.add(new BigDecimal("1"));
+                        		a = a.pow(10, mc);
+                        		a = a.multiply(amount, mc);
+            					return a.subtract(amount, mc);
+            				}
+            			} else {
+            				break;
+            			}
+            		}
+            		b = new BigDecimal(new BigDecimal("0.05").divide(new BigDecimal("365"), mc).toString());
+					b = b.add(new BigDecimal("1"));
+            		b = b.pow(365, mc);
+            		b = b.multiply(amount, mc);
+            		result = amount.multiply(new BigDecimal("0.05"), mc);
+            		return b.subtract(amount, mc);
+            	}
         }
+        return null;
     }
 
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
+    public BigDecimal sumTransactions() {
+        BigDecimal amount = new BigDecimal("0.0");
         for (Transaction t: transactions)
-            amount += t.amount;
+        	amount = amount.add(t.amount);
         return amount;
     }
 
