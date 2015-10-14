0b5bc9ece1db22a557c9acc42ab3053175eb6a1e
jimmy22666 <jimmy22666@hotmail.co.uk>
2015-10-14 12:41:27 +0100

    Update DateProvider.java

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -4,15 +4,22 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateProvider {
-    private static DateProvider instance = null;
+	private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	public static DateProvider getInstance () {
+		if (instance == null)
+			instance = new DateProvider();
+		return instance;
+	}
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	public Date now () {
+		return Calendar.getInstance().getTime();
+	}
+
+	public Date tenAgo () {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(now());
+		cal.add(Calendar.DATE, - 10);
+		return cal.getTime();
+	}
 }
