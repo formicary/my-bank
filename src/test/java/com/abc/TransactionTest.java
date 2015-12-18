e9bf4ab61dd9e506bf7a8ee203b9bd41be17aaa5
James  Lane <james.lane14@imperial.ac.uk>
2015-12-18 10:36:29 +0000

    Added tests for Transactions

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,32 @@
 package com.abc;
 
+import java.util.Calendar;
+
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
+
+  private static final double DOUBLE_DELTA = 1e-15;
+
   @Test
   public void transaction() {
-    Transaction t = new Transaction(5);
+    Transaction t = new Transaction(5.0);
     assertTrue(t instanceof Transaction);
   }
+
+  @Test
+  public void transactionAmount() {
+    Transaction t = new Transaction(5.0);
+    assertEquals(5.0, t.getAmount(), DOUBLE_DELTA);
+  }
+
+  @Test
+  public void transactionDate() {
+    Transaction t = new Transaction(2.0);
+    assertEquals(t.getDate().compareTo(Calendar.getInstance().getTime()), 0);
+  }
+
 }
