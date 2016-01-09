b3605d3a2ed623be56a5d7e19f01ad544809c159
yll12 <yong.loh12@imperial.ac.uk>
2016-01-09 16:50:51 +0000

    Update git ignore

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -41,9 +41,6 @@ public class Account {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
             case MAXI_SAVINGS:
                 if (amount <= 1000)
                     return amount * 0.02;
