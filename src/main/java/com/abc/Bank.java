55b74e9a73b943a80c33496f46960b697946e48e
harry0704 <harry890704@gmail.com>
2016-03-01 16:56:50 +0000

    Update Change String concatenation to StringBuffer
    1. Changed String concatenation to String buffer for performance.
    2. Deleted getFirstCustomer method

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,46 +1,64 @@
 package com.abc;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a application that simulate the basic bank account business functions
+ * 
+ * @author others and Fei
+ * 
+ */
 public class Bank {
-    private List<Customer> customers;
+	private List<Customer> customers;
 
-    public Bank() {
-        customers = new ArrayList<Customer>();
-    }
+	public Bank() {
+		customers = new ArrayList<Customer>();
+	}
 
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
+	public void addCustomer(Customer customer) {
+		customers.add(customer);
+	}
 
-    public String customerSummary() {
-        String summary = "Customer Summary";
-        for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
-    }
+	/**
+	 * This methods return the account number summary for all existing user that
+	 * registered in the bank.
+	 * 
+	 * @return String Customer Summary as a string
+	 */
+	public String customerSummary() {
+		// Use StringBuffer for better performance than concatenation.
+		StringBuffer str = new StringBuffer("Customer Summary");
+		for (Customer c : customers) {
+			str.append("\n - " + c.getName() + " ("
+					+ format(c.getNumberOfAccounts(), "account") + ")");
+		}
+		
+		return str.toString();
+	}
 
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
-    }
+	// Make sure correct plural of word is created based on the number passed
+	// in:
+	// If number passed in is 1 just return the word otherwise add an 's' at the
+	// end
+	private String format(int number, String word) {
+		return number + " " + (number <= 1 ? word : word + "s");
+	}
 
-    public double totalInterestPaid() {
-        double total = 0;
-        for(Customer c: customers)
-            total += c.totalInterestEarned();
-        return total;
-    }
+	/**
+	 * Calculate the total interest paid by the bank. Including all users and
+	 * all the accounts for each user. only keep two digits after decimal point
+	 * 
+	 * @return double This is the total interest paid by bank until today.
+	 */
+	public double totalInterestPaid() {
+		double total = 0;
+		DecimalFormat twoDecimals = new DecimalFormat("#.##");
+        for (Customer c : customers){
+			total += c.totalInterestEarned();
+		}
+		return Double.valueOf(twoDecimals.format(total));
+	}
 
-    public String getFirstCustomer() {
-        try {
-            customers = null;
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
-    }
 }
