ffeb4ce58b6298fe3fd5a401eb742d95bd2db6f2
harry0704 <harry890704@gmail.com>
2016-03-01 17:11:57 +0000

    Update

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -2,12 +2,20 @@ package com.abc;
 
 import org.junit.Test;
 
+import com.abc.Transaction;
+
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Transaction test 
+ * 
+ * @author Others and Fei
+ *
+ */
 public class TransactionTest {
-    @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
-    }
+	@Test
+	public void transaction() {
+		Transaction t = new Transaction(500, "Deposit");
+		assertTrue(t instanceof Transaction);
+	}
 }
