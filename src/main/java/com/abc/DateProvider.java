c1b700ab6f2f1eafd083b83069a3ee300266ed9f
harry0704 <harry890704@gmail.com>
2016-03-01 17:00:39 +0000

    New function added
    1. Add a new function to calculate the days between two date

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -3,16 +3,40 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
-public class DateProvider {
-    private static DateProvider instance = null;
+/**
+ * Provide current date for transactions.
+ * Calculate days between two date
+ * 
+ * @author others and Fei
+ *
+ */
+public final class DateProvider {
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	private static DateProvider instance = null;
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	public static DateProvider getInstance() {
+		if (instance == null)
+			instance = new DateProvider();
+		return instance;
+	}
+
+	/**
+	 * @return Date Current time
+	 */
+	public Date now() {
+		return Calendar.getInstance().getTime();
+	}
+
+	/**
+	 * Calculate the number of days between two date
+	 * 
+	 * @param from Date The start date 
+	 * @param to Date The end date
+	 * @return int The number of days between from and to
+	 */
+	public int diffDays(Date from, Date to) {
+		long diff = to.getTime() - from.getTime();
+		int days = (int) (diff / (1000 * 60 * 60 * 24));
+		return days;
+	}
 }
