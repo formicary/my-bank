a7b9b480059f29624048be54e5fc81f9602f8c8c
harry0704 <harry890704@gmail.com>
2016-03-01 16:52:51 +0000

    Update Account class
    1. Change it to abstract class.
    2. Use synchronized method for deposit, withdraw and transfer to avoid race condition
    3. Added the Account transfer function
    4. Changed some variables
    5. Comment on the codes for further development

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -3,71 +3,130 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Account {
+/**
+ * Abstract class of account. A specific account type need to extend this class.
+ * 
+ * @author Others and Fei
+ * 
+ */
+public abstract class Account {
+    //Account balance
+	private double balance; 
+	//All the transactions of this account
+	private List<Transaction> transactions;
+	//Name of the account
+	private String accountName;
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
+	// Initial account balance are set to 0.0
+	Account() {
+		this.balance = 0.0;
+		transactions = new ArrayList<Transaction>();
+	}
 
-    private final int accountType;
-    public List<Transaction> transactions;
+	public double getBalance() {
+		return balance;
+	}
 
-    public Account(int accountType) {
-        this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
-    }
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
 
-    public void deposit(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("amount must be greater than zero");
-        } else {
-            transactions.add(new Transaction(amount));
-        }
-    }
+	/**
+	 * This method is used to deposit money in bank. Synchronized method to
+	 * avoid race condition on the account modifying.
+	 * 
+	 * @param amount
+	 *            double Amount for deposit. Must be positive
+	 * @return boolean True if deposit success
+	 */
+	public synchronized boolean deposit(double amount) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException(
+					"amount must be greater than zero");
+		} else {
+			this.doTransaction(amount, "deposit");
+			return true;
+		}
+	}
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+	/**
+	 * This method is used to withdraw money from bank. Synchronized method to
+	 * avoid race condition on the account modifying
+	 * 
+	 * @param amount
+	 *            double The amount of withdraw
+	 * @return boolean return true if withdraw success and false if fail
+	 */
+	public synchronized boolean withdraw(double amount) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException(
+					"amount must be greater than zero");
+		} else {
+			// Can't withdraw more money than the account balance.
+			if (amount > balance) {
+				//Tell user the amount they want to withdraw exceed the balance
+				//If there are other ways to show user messages, change here
+				System.out
+						.println("Withdraw amount exceed available funds in the account.");
+				return false;
+			} else {
+				this.doTransaction(-amount, "withdraw");
+				return true;
+			}
+		}
+	}
 
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
-        }
-    }
+	/**
+	 * Transfer money between accounts. Synchronized method to avoid race
+	 * condition on the account modifying
+	 * 
+	 * @param amount
+	 *            double The amount for transfer.
+	 * @param account
+	 *            Account The account to which the money transfer.
+	 */
+	public synchronized boolean transfer(double amount, Account account) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException(
+					"amount must be greater than zero");
+			// The transfer amount must not be larger than available balance.
+		} else if (amount <= balance) {
+			this.doTransaction(-amount, "To " + account.getAccountName());
+			account.doTransaction(amount, "From " + this.getAccountName());
+			return true;
+		} else {
+			//Tell user they have insufficient funds to do transfer
+			//If there are other ways to show user messages, change here
+			System.out.println("Insufficient funds in your account.");
+			return false;
+		}
 
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
+	}
 
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
-        return amount;
-    }
+	/**
+	 * This is the actual method that do the transaction Synchronized method to
+	 * avoid race condition on the account modifying
+	 * 
+	 * @param amount
+	 *            double Amount for transaction
+	 * @param type
+	 *            String To indicate the transaction type.
+	 */
+	public synchronized void doTransaction(double amount, String type) {
+		this.balance += amount;
+		this.transactions.add(new Transaction(amount, type));
+	}
 
-    public int getAccountType() {
-        return accountType;
-    }
+	// Abstract method must me implemented. Different accounts have different
+	// interest calculation formula
+	public abstract double interestEarned();
+
+	public String getAccountName() {
+		return this.accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
 
 }
