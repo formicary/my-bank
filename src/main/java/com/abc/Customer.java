bff410eccb1454fde5891deb6558d1d0a62550e4
harry0704 <harry890704@gmail.com>
2016-03-01 16:59:19 +0000

    Update Customer class
    1. OpenAccount check added
    2. StringBuffer used for performance
    3. Comments added for all methods

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,78 +1,137 @@
 package com.abc;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Math.abs;
 
+/**
+ * Customer class represent each customer of the bank
+ * 
+ * @author Others and Fei
+ * 
+ */
 public class Customer {
-    private String name;
-    private List<Account> accounts;
+	private String name;
+	private List<Account> accounts;
 
-    public Customer(String name) {
-        this.name = name;
-        this.accounts = new ArrayList<Account>();
-    }
+	/**
+	 * Customer has a name and accounts
+	 * 
+	 * @param name
+	 */
+	public Customer(String name) {
+		this.name = name;
+		this.accounts = new ArrayList<Account>();
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Customer openAccount(Account account) {
-        accounts.add(account);
-        return this;
-    }
+	/**
+	 * For user to open one type of account that he/she has not open yet.
+	 * 
+	 * @param account
+	 * @return Customer
+	 */
+	public Customer openAccount(Account account) {
+		// Check if user has opened the account before
+		// Each customer can only have only one account of each type
+		for (Account a : accounts) {
+			if (account.getAccountName().equals(a.getAccountName())) {
+				//Tell user they have already opened the account
+				//If there are other ways to show user messages, change here
+				System.out.println("You have already opened a "
+						+ account.getAccountName());
+				return this;
+			}
+		}
+		accounts.add(account);
+		//Tell user they successfully opened the account
+		//If there are other ways to show user messages, change here
+		System.out.println("You have successfully opened a "
+				+ account.getAccountName() + ".");
+		return this;
+	}
 
-    public int getNumberOfAccounts() {
-        return accounts.size();
-    }
+	public int getNumberOfAccounts() {
+		return accounts.size();
+	}
 
-    public double totalInterestEarned() {
-        double total = 0;
-        for (Account a : accounts)
-            total += a.interestEarned();
-        return total;
-    }
+	/**
+	 * This method calculate the interest earned by the user since the first
+	 * successful transaction. It will calculate all the opened account.
+	 * 
+	 * @return double The total interest
+	 */
+	public double totalInterestEarned() {
+		double total = 0;
+		for (Account a : accounts) {
+			total += a.interestEarned();
+		}
+		DecimalFormat twoDecimals = new DecimalFormat("#.##");
+		return Double.valueOf(twoDecimals.format(total));
+	}
 
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
-    }
+	/**
+	 * Get the account with the specific name. The name is not case sensitive.
+	 * 
+	 * @param accountName
+	 * @return Account
+	 */
+	public Account getAccount(String accountName) {
+		for (Account a : accounts) {
+			if (a.getAccountName().equalsIgnoreCase(accountName)) {
+				return a;
+			}
+		}
+		//Tell user they haven't opened the account
+		//If there are other ways to show user messages, change here
+		System.out.println("You haven't open the " + accountName + ".");
+		return null;
+	}
 
-    private String statementForAccount(Account a) {
-        String s = "";
+	/**
+	 * Get the bank statement for user of all accounts.
+	 * 
+	 * @return String user bank statement
+	 */
+	public String getStatement() {
+		// StringBuffer used for better performance
+		StringBuffer str = new StringBuffer();
+		str.append("Statement for " + this.name + "\n");
+		double total = 0.0;
+		for (Account a : accounts) {
+			str.append("\n" + statementForAccount(a) + "\n");
+			total += a.getBalance();
+		}
+		str.append("\nTotal In All Accounts " + toDollars(total));
+		return str.toString();
+	}
 
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
+	/**
+	 * This method gets the bank statement for the input account.
+	 * 
+	 * @param a Account Each account of user
+	 * @return String Statement for the input account
+	 */
+	public String statementForAccount(Account a) {
+		StringBuilder str = new StringBuilder();
+		str.append(a.getAccountName() + "\n");
+		double total = 0.0;
+		//Iterate through the transactions to generate statement.
+		for (Transaction t : a.getTransactions()) {
+			str.append("  " + t.getType() + " " + toDollars(t.getAmount())
+					+ "\n");
+			total += t.getAmount();
+		}
+		str.append("Total " + toDollars(total));
+		return str.toString();
+	}
 
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
-        return s;
-    }
-
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
-    }
+	private String toDollars(double d) {
+		return String.format("$%,.2f", abs(d));
+	}
 }
