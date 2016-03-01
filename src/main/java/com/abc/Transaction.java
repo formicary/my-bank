d64cab3af5f9b08b70b906b490944972e59eb737
harry0704 <harry890704@gmail.com>
2016-03-01 17:02:38 +0000

    Update
    1. Constructor updated 
    2. Comments added

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,59 @@
 package com.abc;
 
-import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * Transaction class denote each transaction of account
+ * 
+ * @author Fei
+ *
+ */
 public class Transaction {
-    public final double amount;
-
-    private Date transactionDate;
-
-    public Transaction(double amount) {
+	//The transaction amount
+    private final double amount;
+    //The date of transaction
+    private final Date transactionDate;
+    //The transaction type e.g. withdraw, deposit, transfer from/to
+    private final String type;
+    
+    /**
+     * Transaction constructor
+     * using current date as transaction date
+     * 
+     * @param amount double Amount of transaction 
+     * @param type String Type of transaction
+     */
+    public Transaction(double amount,String type) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
+        this.type = type;
     }
+    
+    /**
+     * Transaction constructor
+     * date defined by user.
+     * 
+     * @param amount double Amount of transaction
+     * @param transactionDate Date Date of transaction
+     * @param type String Transaction type
+     */
+    public Transaction(double amount,Date transactionDate,String type){
+    	this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.type = type;
+    }
+    
+    
+    public double getAmount(){
+    	return amount;
+    }
+    
+    public Date getDate(){
+    	return transactionDate;
+    }
+
+	public String getType() {
+		return type;
+	}
 
 }
