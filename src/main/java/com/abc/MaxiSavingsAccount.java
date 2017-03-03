5b88df558017689eb73c87bbb459cf0223a01906
softlull <softlull@users.noreply.github.com>
2017-03-03 09:53:06 +0000

    update interestEarned method in MaxiSavingsAccount

--- src/main/java/com/abc/MaxiSavingsAccount.java
+++ src/main/java/com/abc/MaxiSavingsAccount.java
@@ -13,13 +13,13 @@ public class MaxiSavingsAccount extends SavingsAccount {
 	@Override
     public double interestEarned() {
         double amount = sumTransactions();
-	    if (amount <= 1000) {
-	    	return amount * 0.001;
-	    }
-	          
-	    else {
-	    	return 1 + (amount-1000) * 0.002;
-	    }	          
+        if (amount <= 1000) {
+            return amount * 0.02;
+        }
+        if (amount <= 2000) {
+            return 20 + (amount-1000) * 0.05;
+        }
+        return 70 + (amount-2000) * 0.1;          
     }
 
 }
