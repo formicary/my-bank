5500a5fe5b17b86b8dc15ea1bf877deca8355627
Afterburner <dragosd2000@gmail.com>
2017-05-11 17:42:39 +0100

    Cleaned up code, added tests and added inter-account transfer, changed maxi-savings rate and working on interest accrual

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -3,6 +3,7 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.abc.AccountType.*;
 import static java.lang.Math.abs;
 
 public class Customer {
@@ -34,42 +35,65 @@ public class Customer {
         return total;
     }
 
+    public void deposit(Account a, double value) {
+        if (accounts.contains(a)) {
+            a.deposit(value);
+        }
+    }
+
+    public void withdraw(Account a, double value) {
+        if (accounts.contains(a)) {
+            a.withdraw(value);
+        }
+    }
+
+    public void transfer(Account a, Account b, double value) {
+        ArrayList<Account> transferAccounts = new ArrayList<Account>();
+        transferAccounts.add(a);
+        transferAccounts.add(b);
+        if (accounts.containsAll(transferAccounts)) {
+            a.withdraw(value);
+            b.deposit(value);
+        }
+    }
+
     public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
+        StringBuilder statementBuilder = new StringBuilder();
+        statementBuilder.append("Statement for " + this.name + "\n");
+
         double total = 0.0;
         for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
+            statementBuilder.append("\n" + statementForAccount(a) + "\n");
             total += a.sumTransactions();
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
+        statementBuilder.append("\nTotal In All Accounts " + toDollars(total));
+        return statementBuilder.toString();
     }
 
     private String statementForAccount(Account a) {
-        String s = "";
+        StringBuilder statementBuilder = new StringBuilder();
 
        //Translate to pretty account type
         switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
+            case CHECKING:
+                statementBuilder.append("Checking Account\n");
                 break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
+            case SAVINGS:
+                statementBuilder.append("Savings Account\n");
                 break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
+            case MAXI_SAVINGS:
+                statementBuilder.append("Maxi Savings Account\n");
                 break;
         }
 
         //Now total up all the transactions
         double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
+        for (Transaction t : a.getTransactions()) {
+            statementBuilder.append("  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n");
             total += t.amount;
         }
-        s += "Total " + toDollars(total);
-        return s;
+        statementBuilder.append("Total " + toDollars(total));
+        return statementBuilder.toString();
     }
 
     private String toDollars(double d){
--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -3,24 +3,70 @@ package com.abc;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BankTest {
+    @Test
+    public void addCustomer() throws Exception {
+        Bank bank = new Bank();
+        Customer jack = new Customer("Jack");
+        jack.openAccount(new Account(AccountType.CHECKING));
+        bank.addCustomer(jack);
+        assertTrue(bank.getFirstCustomer().equals("Jack"));
+    }
+
+    @Test
+    public void totalInterestPaid() throws Exception {
+        Bank bank = new Bank();
+        Customer jack = new Customer("Jack");
+        Account jacksCheckingAcc = new Account(AccountType.CHECKING);
+        jack.openAccount(jacksCheckingAcc);
+        bank.addCustomer(jack);
+        jack.deposit(jacksCheckingAcc, 300.0);
+        assertTrue(bank.totalInterestPaid() == 0.3);
+
+        Account jacksSavingsAcc = new Account(AccountType.SAVINGS);
+        jack.openAccount(jacksSavingsAcc);
+        jack.deposit(jacksSavingsAcc, 1001);
+        assertTrue(bank.totalInterestPaid() == 1.302);
+
+    }
+
+    @Test
+    public void getFirstCustomer() throws Exception {
+        addCustomer();
+    }
+
     private static final double DOUBLE_DELTA = 1e-15;
+
+    @Test
+    public void customerSummaries() {
+        Bank bank = new Bank();
+        Customer john = new Customer("John");
+        john.openAccount(new Account(AccountType.CHECKING));
+        bank.addCustomer(john);
+        Customer babyGroot = new Customer("Baby Groot");
+        babyGroot.openAccount(new Account(AccountType.CHECKING));
+        bank.addCustomer(babyGroot);
+        assertEquals("Customer Summaries\n - John (1 account)\n - Baby Groot (1 account)", bank.customerSummaries());
+    }
 
     @Test
     public void customerSummary() {
         Bank bank = new Bank();
         Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
+        john.openAccount(new Account(AccountType.CHECKING));
         bank.addCustomer(john);
-
-        assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
+        Customer babyGroot = new Customer("Baby Groot");
+        babyGroot.openAccount(new Account(AccountType.CHECKING));
+        bank.addCustomer(babyGroot);
+        assertEquals("Customer Summary\n - Baby Groot (1 account)", bank.customerSummary(babyGroot));
     }
 
     @Test
     public void checkingAccount() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.CHECKING);
+        Account checkingAccount = new Account(AccountType.CHECKING);
         Customer bill = new Customer("Bill").openAccount(checkingAccount);
         bank.addCustomer(bill);
 
@@ -32,7 +78,7 @@ public class BankTest {
     @Test
     public void savings_account() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new Account(AccountType.SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
         checkingAccount.deposit(1500.0);
@@ -43,12 +89,12 @@ public class BankTest {
     @Test
     public void maxi_savings_account() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        Account checkingAccount = new Account(AccountType.MAXI_SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
         checkingAccount.deposit(3000.0);
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        assertEquals(3.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
 }
