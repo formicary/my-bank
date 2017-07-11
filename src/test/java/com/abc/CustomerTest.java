f1a4f29d86ecb152241c967fb917d04eb5d84f5d
Robert Powell Davies <robpdavies@gmail.com>
2017-07-11 18:30:15 +0100

    Customer
    changed name variable to final
    changed accounts from list to map to allow AccountID method of choosing Account
    openAccount changed to void
    refactored all string creating methods to use StringBuilder
    added getAmount method to Transaction class
    moved toDollars to BankUtils class
    added Customer tests

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -1,5 +1,6 @@
 package com.abc;
 
+import static com.abc.Account.AccountType.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -7,51 +8,67 @@ import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
 
-    @Test //Test customer statement generation
-    public void testApp(){
+    @Test
+    public void testOpenAccount1() {
+        Customer oscar = new Customer("Oscar");
 
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
+        oscar.openAccount("AccSavings", new Account(SAVINGS));
+        oscar.openAccount("AccMaxi", new Account(MAXI_SAVINGS));
+    }
 
-        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
+    @Test(expected = IllegalArgumentException.class)
+    public void testOpenAccount2() {
+        Customer oscar = new Customer("Oscar");
+
+        oscar.openAccount("AccSavings", new Account(SAVINGS));
+        oscar.openAccount("AccSavings", new Account(CHECKING));
+    }
+
+    @Test
+    public void testGetNumberOfAccounts() {
+        Customer oscar = new Customer("Oscar");
+
+        oscar.openAccount("Acc1", new Account(SAVINGS));
+
+        int expected = 1;
+        int result = oscar.getNumberOfAccounts();
+        assertEquals(expected, result);
+
+        oscar.openAccount("Acc2", new Account(CHECKING));
+
+        expected = 2;
+        result = oscar.getNumberOfAccounts();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetStatement() {
+        Customer oscar = new Customer("Oscar");
+
+        Account checkingAccount = new Account(CHECKING);
+        Account savingsAccount = new Account(SAVINGS);
+
+        oscar.openAccount("AccChecking", checkingAccount);
+        oscar.openAccount("AccSavings", savingsAccount);
 
         checkingAccount.deposit(100.0);
         savingsAccount.deposit(4000.0);
         savingsAccount.withdraw(200.0);
 
-        assertEquals("Statement for Henry\n" +
-                "\n" +
-                "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
-                "\n" +
-                "Savings Account\n" +
-                "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
-                "Total $3,800.00\n" +
-                "\n" +
-                "Total In All Accounts $3,900.00", henry.getStatement());
-    }
+        String expected = "Statement for Oscar\n"
+                + "\n"
+                + "Savings Account\n"
+                + "  deposit $4,000.00\n"
+                + "  withdrawal $200.00\n"
+                + "Total $3,800.00\n"
+                + "\n"
+                + "Checking Account\n"
+                + "  deposit $100.00\n"
+                + "Total $100.00\n"
+                + "\n"
+                + "Total In All Accounts $3,900.00";
+        String result = oscar.getStatement();
 
-    @Test
-    public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
-        assertEquals(1, oscar.getNumberOfAccounts());
-    }
-
-    @Test
-    public void testTwoAccount(){
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
-        assertEquals(2, oscar.getNumberOfAccounts());
-    }
-
-    @Ignore
-    public void testThreeAcounts() {
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
-        assertEquals(3, oscar.getNumberOfAccounts());
+        assertEquals(expected, result);
     }
 }
