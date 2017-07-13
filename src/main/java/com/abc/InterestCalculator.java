beab923af5ed8585e1132c9169ea9b329b7c0c12
Robert Powell Davies <robpdavies@gmail.com>
2017-07-13 08:55:00 +0100

    Interest Calculator -Code cleaned

--- src/main/java/com/abc/InterestCalculator.java
+++ src/main/java/com/abc/InterestCalculator.java
@@ -19,54 +19,80 @@ import java.util.List;
 public class InterestCalculator {
 
     List<Transaction> transactions;
+    AccountType accountType;
 
-    public InterestCalculator(List<Transaction> transactions) {
+    public InterestCalculator(List<Transaction> transactions, AccountType accountType) {
         this.transactions = transactions;
         //All transactions should already be in chronological order, this confirms it
         Collections.sort(this.transactions);
+        this.accountType = accountType;
     }
 
-    public double calcCompoundInterest(AccountType accountType) throws ParseException {
-        return checkingInterest();       
-    }
-
-    private double checkingInterest() throws ParseException {
-        Transaction currentT = null;
+ 
+    public double calcCompoundInterest() throws ParseException {
         double accountTotal = 0.0;
-        int differenceInDays;
-        double interestRate = 0.1;
+        int daysSinceLastWithdrawal = 0;
 
-        for (Transaction nextT : transactions) {
-            if (currentT != null) {
-                differenceInDays = DateProvider.differenceInDays(currentT.getDate(), nextT.getDate());
-                accountTotal += currentT.getAmount();
-                accountTotal = calcCheckingInterest(accountTotal, differenceInDays, interestRate);
+        for (int i = 0; i < transactions.size() - 1; i++) {
+            Transaction currentT = transactions.get(i);
+            Date nextDate = transactions.get(i + 1).getDate();
+            
+            if (currentT.getAmount() < 0) {
+                daysSinceLastWithdrawal = 0;
             }
-            currentT = nextT;
+
+            accountTotal = calculateTransactionPeriod(accountTotal, currentT, nextDate, daysSinceLastWithdrawal);
+            daysSinceLastWithdrawal += DateUtils.differenceInDays(currentT.getDate(), nextDate);;
         }
-        //Test code
+
+        Transaction lastT = transactions.get(transactions.size() - 1);
+        if (lastT.getAmount() < 0) {
+            daysSinceLastWithdrawal = 0;
+        }
+
+        //Test code for DateProvider.now() injection
         SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
         String testDateInString = "30-01-2000 00:00:00";
         Date testDate = sdf.parse(testDateInString);
-        differenceInDays = DateProvider.differenceInDays(currentT.getDate(), testDate);
-         
+        accountTotal = calculateTransactionPeriod(accountTotal, lastT, testDate, daysSinceLastWithdrawal);
         
-        //differenceInDays = DateProvider.differenceInDays(currentT.getDate(), DateProvider.now());
-        accountTotal += currentT.getAmount();
-        accountTotal = calcCheckingInterest(accountTotal, differenceInDays, interestRate);
-
+        //accountTotal = calculateTransactionPeriod(accountTotal, lastT, DateUtils.now(), daysSinceLastWithdrawal);
         return accountTotal - sumTransactions();
     }
 
-    public double calcCheckingInterest(double amount, int days, double yearlyInterestRate) {
-        double dailyInterestRate = calculateDailyInterestRate(yearlyInterestRate);
-        
-        for (int i = 0; i < days; i++){
-            amount = amount * (1+dailyInterestRate);
+    private double calculateTransactionPeriod(double accountTotal, Transaction transaction, Date nextTransactionDate, int daysSinceLastWithdrawal) {
+        int daysBeforeNextTransaction = DateUtils.differenceInDays(transaction.getDate(), nextTransactionDate);
+        accountTotal += transaction.getAmount();
+
+        return calcPeriodInterest(accountTotal, daysBeforeNextTransaction, daysSinceLastWithdrawal);
+    }
+
+    public double calcPeriodInterest(double amount, int days, int daysSinceLastWithdrawal) {
+        for (int i = 0; i < days; i++) {
+            amount = calcDaysInterest(amount, daysSinceLastWithdrawal);
+            daysSinceLastWithdrawal++;
         }
-        
         return amount;
-        //return amount * Math.pow((1 + dailyInterestRate), days);
+    }
+
+    private double calcDaysInterest(double amount, int daysSinceLastWithdrawal) {
+        switch (accountType) {
+            case CHECKING:
+                return amount * (1 + calculateDailyInterestRate(0.1));
+            case SAVINGS:
+                if (amount <= 1000.0) {
+                    return amount * (1 + calculateDailyInterestRate(0.1));
+                } else {
+                    return ((amount - 1000) * (1 + calculateDailyInterestRate(0.2)) + 1000 * (1 + calculateDailyInterestRate(0.1)));
+                }
+            case MAXI_SAVINGS:
+                if (daysSinceLastWithdrawal < 10) {
+                    return amount * (1 + calculateDailyInterestRate(0.1));
+                } else {
+                    return amount * (1 + calculateDailyInterestRate(5));
+                }
+        }
+        return 0.0;
     }
 
     public double sumTransactions() {
@@ -78,7 +104,7 @@ public class InterestCalculator {
     }
 
     private double calculateDailyInterestRate(double yearlyInterestRate) {
-        return yearlyInterestRate / 365;
+        return (yearlyInterestRate / 365) / 100;
     }
 
 }
