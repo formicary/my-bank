90e3132d9080ee6e5dde6ed033f6465259393fee
Robert Powell Davies <robpdavies@gmail.com>
2017-07-13 10:32:33 +0100

    -Code cleanup -fully tested

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,73 +1,100 @@
 package com.abc;
 
+import com.abc.utils.DateUtils;
+import com.abc.utils.StringUtils;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Account {
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
+    public enum AccountType {
+        CHECKING,
+        SAVINGS,
+        MAXI_SAVINGS
+    }
 
-    private final int accountType;
-    public List<Transaction> transactions;
+    private final AccountType accountType;
+    private final List<Transaction> transactions;
+    private final InterestCalculator interestCalculator;
 
-    public Account(int accountType) {
+    public Account(AccountType accountType) {
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
+        this.interestCalculator = new InterestCalculator(transactions, accountType);
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
     }
 
     public void deposit(double amount) {
+        deposit(amount, DateUtils.now());
+    }
+
+    public void deposit(double amount, Date date) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
         } else {
-            transactions.add(new Transaction(amount));
+            transactions.add(new Transaction(amount, date));
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
+    public boolean withdraw(double amount) {
+        return withdraw(amount, DateUtils.now());
     }
-}
-
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
+   
+    public boolean withdraw(double amount, Date date) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        } else {
+            if (sumTransactions() > amount) {
+                transactions.add(new Transaction(-amount, date));
+                return true;
+            }
+            return false;
         }
+    }
+
+    public double interestEarned(Date dateToCalculateTo) {
+        return interestCalculator.calcCompoundInterest(dateToCalculateTo);
     }
 
     public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
         double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
+        for (Transaction t : transactions) {
+            amount += t.getAmount();
+        }
         return amount;
     }
 
-    public int getAccountType() {
-        return accountType;
+    public String statement() {
+        StringBuilder sb = new StringBuilder();
+
+        //Translate to pretty account type
+        switch (getAccountType()) {
+            case CHECKING:
+                sb.append("Checking Account\n");
+                break;
+            case SAVINGS:
+                sb.append("Savings Account\n");
+                break;
+            case MAXI_SAVINGS:
+                sb.append("Maxi Savings Account\n");
+                break;
+        }
+
+        //Now total up all the transactions
+        double total = 0.0;
+        for (Transaction t : transactions) {
+            sb.append("  ")
+                    .append(t.getAmount() < 0 ? "withdrawal " : "deposit ")
+                    .append(StringUtils.toDollars(t.getAmount()))
+                    .append("\n");
+            total += t.getAmount();
+        }
+        sb.append("Total ").append(StringUtils.toDollars(total));
+        return sb.toString();
     }
 
 }
