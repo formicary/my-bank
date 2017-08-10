ab0d255cdde8c0a9a97b4d58472b233bef48b299
Will3536 <Will3536@hotmail.co.uk>
2017-08-10 23:45:26 +0100

    Create CheckingAccount.java

---
+++ src/main/java/com/abc/CheckingAccount.java
@@ -0,0 +1,23 @@
+public class CheckingAccount extends Account{
+
+	private static double interestRate;
+	
+	public CheckingAccount(){
+		super(AccountType.CHECKING);
+		interestRate = 0.001;
+	}
+
+	public double interestEarned() {
+		double amount = sumTransactions();
+        
+        return amount * interestRate;
+	}
+	
+	public static void setInterestRate(double newInterestRate){
+		interestRate = newInterestRate;
+	}
+	
+	public static double getInterestRate(){
+		return interestRate;
+	}
+}
