d5407dd550ff9ae9dba62cda52b3c94ad1112a4f
joedarby <joedarby87@gmail.com>
2017-10-20 19:42:49 +0100

    Implement compound interest for savingsAccount

--- src/main/java/com/abc/accounts/SavingsAccount.java
+++ src/main/java/com/abc/accounts/SavingsAccount.java
@@ -1,21 +1,51 @@
 package com.abc.accounts;
 
+import com.abc.Transaction;
+
+import java.util.*;
+
 public class SavingsAccount extends Account {
 
     private static double TIER_1_INTEREST_RATE = 0.001;
     private static double TIER_2_INTEREST_RATE = 0.002;
+    private static double TIER_1_DAILY_INTEREST_RATE = TIER_1_INTEREST_RATE / 365.0;
+    private static double TIER_2_DAILY_INTEREST_RATE = TIER_2_INTEREST_RATE / 365.0;
+
 
     public SavingsAccount() {
         super();
     }
 
     public double interestEarned() {
-        double amount = sumTransactions();
-        if (amount <= 1000)
-            return amount * TIER_1_INTEREST_RATE;
-        else
-            return 1 + (amount-1000) * TIER_2_INTEREST_RATE;
+        Collections.sort(getTransactions());
+        Deque<Transaction> transactionDeque = new ArrayDeque<Transaction>(getTransactions());
 
+        Date firstTransaction = transactionDeque.getFirst().transactionDate;
+        Calendar start = new GregorianCalendar();
+        start.setTime(firstTransaction);
+
+        Calendar end = Calendar.getInstance();
+
+        double balance = 0.0;
+        double totalInterest = 0.0;
+
+        for (Date d = firstTransaction; start.before(end);
+             start.add(Calendar.DAY_OF_YEAR, 1), d = start.getTime()) {
+
+            if (!transactionDeque.isEmpty()){
+                Date nextTransaction = transactionDeque.getFirst().transactionDate;
+                if (d.after(nextTransaction) || d.equals(nextTransaction)) {
+                    balance += transactionDeque.removeFirst().amount;
+                }
+            }
+            double dayInterest = balance <= 1000
+                    ? balance * TIER_1_DAILY_INTEREST_RATE
+                    : 1000 * TIER_1_DAILY_INTEREST_RATE + (balance - 1000) * TIER_2_DAILY_INTEREST_RATE;
+            totalInterest += dayInterest;
+            balance += dayInterest;
+
+        }
+        return totalInterest;
     }
 
     protected String getPrettyAccountType() {
--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -3,6 +3,7 @@ package com.abc;
 import com.abc.accounts.Account;
 import com.abc.accounts.CheckingAccount;
 import com.abc.accounts.MaxiSavingsAccount;
+import com.abc.accounts.SavingsAccount;
 import org.junit.Test;
 
 import java.util.Date;
@@ -64,6 +65,23 @@ public class AccountTest {
         checkingAccount.addTransaction(withdrawal);
 
         assertEquals(0.0808, checkingAccount.interestEarned(), 0.01);
+
+    }
+
+    @Test
+    public void savingAccountInterest() {
+        Account savingsAccount = new SavingsAccount();
+
+        Date depositDate = DateProvider.getInstance().earlierDate(30);
+        Transaction deposit = new Transaction(10000, depositDate);
+
+        Date withdrawalDate = DateProvider.getInstance().earlierDate(15);
+        Transaction withdrawal = new Transaction(-9500, withdrawalDate);
+
+        savingsAccount.addTransaction(deposit);
+        savingsAccount.addTransaction(withdrawal);
+
+        assertEquals(0.853, savingsAccount.interestEarned(), 0.01);
 
     }
 
