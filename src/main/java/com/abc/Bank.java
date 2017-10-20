a6f631053d8b687fbf20f27c1fcf09252c26d0bc
tomhendra865 <30497195+tomhendra865@users.noreply.github.com>
2017-10-20 17:32:22 +0100

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -6,37 +6,46 @@ import java.util.List;
 public class Bank {
     private List<Customer> customers;
 
-    public Bank() {
-        customers = new ArrayList<Customer>();
+    protected Bank() {
+        this.customers = new ArrayList<Customer>();
     }
 
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
+    protected void addCustomer(Customer customer) {
+        this.customers.add(customer);
     }
-
-    public String customerSummary() {
+    
+//Make sure correct plural of word is created based on the number passed in:
+    //If number passed in is 1 just return the word otherwise add an 's' at the end
+    private String format(int number, String word) {
+        String output;
+        if (number<1){
+            throw new IllegalArgumentException("Number provided must be greater than or equal to 1");
+        }
+        else if (number==1){
+            output = number +" "+word;
+            return output;
+        } else {
+            output = number+" "+word.append("s");
+            return output;
+        }
+    }
+    
+    protected String customerSummary() {
         String summary = "Customer Summary";
         for (Customer c : customers)
             summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
         return summary;
     }
 
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
-    }
-
-    public double totalInterestPaid() {
-        double total = 0;
+    protected double totalInterestPaid() {
+        double total;
         for(Customer c: customers)
             total += c.totalInterestEarned();
         return total;
     }
 
-    public String getFirstCustomer() {
+    protected String getFirstCustomer() {
         try {
-            customers = null;
             return customers.get(0).getName();
         } catch (Exception e){
             e.printStackTrace();
