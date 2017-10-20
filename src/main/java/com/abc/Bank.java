c703e0be4353f54b4b8c91f1c5baa792d9cf71a8
tomhendra865 <30497195+tomhendra865@users.noreply.github.com>
2017-10-20 19:07:51 +0100

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
+    
     private List<Customer> customers;
 
     protected Bank() {
@@ -13,9 +14,7 @@ public class Bank {
     protected void addCustomer(Customer customer) {
         this.customers.add(customer);
     }
-    
-//Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
+
     private String format(int number) {
         String output;
         if (number==1){
@@ -28,22 +27,24 @@ public class Bank {
     
     protected String customerSummary() {
         String summary = "Customer Summary";
-        for (Customer c : customers)
+        for (Customer c : customers) {
             summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts()) + ")";
+        }
         return summary;
     }
 
     protected double totalInterestPaid() {
         double total;
-        for(Customer c: customers)
+        for(Customer c: customers) {
             total += c.totalInterestEarned();
+        }
         return total;
     }
 
     protected String getFirstCustomer() {
         try {
             return customers.get(0).getName();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "Error";
         }
