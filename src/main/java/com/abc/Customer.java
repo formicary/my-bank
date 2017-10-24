bf8ef0695c8bad2098ec9ddb18e666d0c00238f0
tomhendra865 <30497195+tomhendra865@users.noreply.github.com>
2017-10-24 16:41:00 +0100

    Update Customer.java

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,102 +1,93 @@
 package com.abc;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-import java.math.RoundingMode;
-import java.math.BigDecimal;
 
-import static java.lang.Math.abs;
-
-public class Customer {
+/**
+ * This class allows the creation of a bank class to maintain a list of customers, produce a summary of each customer and produce an account of all interest paid to all customers 
+ * @author tomhendra
+ * @param customers	an arrayList of customers attributed to this bank object
+ */
+public class Bank {
     
-    private String name;
-    private List<Account> accounts;
+    private List<Customer> customers;
 
-    protected Customer(String name) {
-        this.name = name;
-        this.accounts = new ArrayList<Account>();
-    }
-
-    protected String getName() {
-        return name;
-    }
-
-    protected Customer openAccount(Account account) {
-        accounts.add(account);
-        return this;
-    }
-
-    protected int getNumberOfAccounts() {
-        return accounts.size();
-    }
-
-    
-    protected double totalInterestEarned() {
-        double total = 0.00;
-        BigDecimal roundedTotal;
-        BigDecimal convertedTotal;
-        for (Account a : accounts) {
-            total += a.interestEarned();
-        }
-        convertedTotal = BigDecimal.valueOf(total);
-        roundedTotal = convertedTotal.setScale(2, RoundingMode.HALF_EVEN);
-        return roundedTotal.doubleValue();
-    }
-     
-    protected void transfer(double amount, int withdrawalAccountIndex, int depositAccountIndex) {
-        accounts.get(withdrawalAccountIndex).withdraw(amount);
-        accounts.get(depositAccountIndex).deposit(amount);
-    }
-        
-    private String toDollars(double d) {
-        return String.format("$%1$,.2f", abs(d));
+    protected Bank() {
+        this.customers = new ArrayList<Customer>();
     }
     
-    protected String getStatement() {
-        String statement;
-        double total = 0.00;
-        BigDecimal convertedTotal;
-        BigDecimal roundedTotal;
-        statement = String.format("Statement for %1$s%n", name);
-        for (Account a : accounts) {
-            statement += String.format("%n%1$s%n", statementForAccount(a));
-            total += a.sumTransactions();
-        }
-        convertedTotal = BigDecimal.valueOf(total);
-        roundedTotal = convertedTotal.setScale(2, RoundingMode.HALF_EVEN);
-        statement += String.format("%nTotal In All Accounts %1$s", toDollars(roundedTotal.doubleValue()));
-        return statement;
+    /**
+     * Scales the BigDecimal value to 2 decimal places and rounds to 'banking rounding' for calculations when combining big decimal values for readability
+     * @param b BigDecimal to be scaled and rounded
+     * @return the BigDecimal input as a new BigDecimal scaled to 2 decimal places and rounded using the 'bankers rounding' mode
+     */
+    protected static final BigDecimal scale(BigDecimal b) {
+    		b=b.setScale(2, RoundingMode.HALF_EVEN);
+    		return b;
     }
 
-    private String statementForAccount(Account a) {
-        double total = 0.00;
-        BigDecimal convertedTotal;
+    /**
+     * Adds customer objects to this banks customers arrayList
+     * @param customer	customer input object to be added to this arrayList
+     */
+    protected void addCustomer(Customer customer) {
+        this.customers.add(customer);
+    }
+
+    /**
+     * Returns the correct plural or singular term of account depending on the number input
+     * @param number	number of accounts attributed to the bank
+     * @return	the correct plural or singular of account
+     */
+    private String format(int number) {
+        String output;
+        if (number==1) {
+            output = String.format("%1$d %2$s",number, "account");
+        } else {
+            output = String.format("%1$d %2$s", number, "accounts");
+        }
+        return output;
+    }
+    
+    /**
+     * Produces a summary of the name and number of accounts held by each customer
+     * @return summary of all customers in a readable form
+     */
+    protected String customerSummary() {
+        String summary = "Customer Summary";
+        for (Customer c : customers) {
+            summary += String.format("%n - %1$s (%2$s)", c.getName(), format(c.getNumberOfAccounts()));
+        }
+        return summary;
+    }
+
+    /**
+     * calculates and returns the total interest paid by the bank object to each attributed customer
+     * @param convertedTotal	total value of interest paid by bank
+     * @return total interest paid by bank as a BigDecimal
+     */
+    protected BigDecimal totalInterestPaid() {
         BigDecimal roundedTotal;
-        String statement;
-        switch (a.getAccountType()) {
-            case Account.CHECKING:
-                statement = "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-            		statement = "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-            		statement = "Maxi Savings Account\n";
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid account type");
+        roundedTotal = BigDecimal.ZERO;
+        roundedTotal = scale(roundedTotal);
+        for(Customer c: customers) {
+            roundedTotal = roundedTotal.add(c.totalInterestEarned());
         }
-        for (Transaction t : a.transactions) {
-            if (t.amount < 0) {
-            		statement += String.format("  withdrawal %1$s%n", toDollars(t.amount));
-            } else {
-            		statement += String.format("  deposit %1$s%n", toDollars(t.amount));
-            }
-            total += t.amount;
+        return roundedTotal;
+    }
+
+    /**
+     * Returns the first indexed customer in the arrayList, if there are no customers added, a message stating the problem is returned
+     * @return
+     */
+    protected String getFirstCustomer() {
+        try {
+            return customers.get(0).getName();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "No customer found";
         }
-        convertedTotal = BigDecimal.valueOf(total);
-        roundedTotal = convertedTotal.setScale(2, RoundingMode.HALF_EVEN);
-        statement += String.format("Total %1$s", toDollars(roundedTotal.doubleValue()));
-        return statement;
     }
 }
