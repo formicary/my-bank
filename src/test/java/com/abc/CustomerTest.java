cc31bdce7383a640dc8b43560f98b9f7d7d495db
tomhendra865 <30497195+tomhendra865@users.noreply.github.com>
2017-10-24 12:21:37 +0100

    Update CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -4,21 +4,33 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
 public class CustomerTest {
 
+	@Test //Test customer object generation
+    public void testObj() {
+		Account checkingAccount = new Account(Account.CHECKING);
+		Account savingsAccount = new Account(Account.SAVINGS);
+		Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
+        	Customer sarah = new Customer("Sarah").openAccount(checkingAccount).openAccount(savingsAccount);
+        	
+        	assertNotNull(henry);
+        assertNotSame(henry, sarah);
+	}
+    
     @Test //Test customer statement generation
-    public void testApp(){
-
+    public void testApp() {
         Account checkingAccount = new Account(Account.CHECKING);
         Account savingsAccount = new Account(Account.SAVINGS);
-
         Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
-
+        
         checkingAccount.deposit(100.0);
         savingsAccount.deposit(4000.0);
         savingsAccount.withdraw(200.0);
-
+        
         assertEquals("Statement for Henry\n" +
                 "\n" +
                 "Checking Account\n" +
@@ -31,20 +43,23 @@ public class CustomerTest {
                 "Total $3,800.00\n" +
                 "\n" +
                 "Total In All Accounts $3,900.00", henry.getStatement());
+        assertFalse("Incomplete statement" == henry.getStatement());
     }
 
     @Test
-    public void testOneAccount(){
+    public void testOneAccount() {
         Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
         assertEquals(1, oscar.getNumberOfAccounts());
+        assertFalse(2 == oscar.getNumberOfAccounts());
     }
 
     @Test
-    public void testTwoAccount(){
+    public void testTwoAccount() {
         Customer oscar = new Customer("Oscar")
                 .openAccount(new Account(Account.SAVINGS));
         oscar.openAccount(new Account(Account.CHECKING));
         assertEquals(2, oscar.getNumberOfAccounts());
+        assertFalse(3 == oscar.getNumberOfAccounts());
     }
 
     @Ignore
