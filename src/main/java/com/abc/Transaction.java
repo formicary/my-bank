051dd2d0b92013306279dc2cb898e9e7e61b7d07
mam458 <mam458@student.bham.ac.uk>
2018-01-23 18:22:55 +0000

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -8,9 +8,19 @@ public class Transaction {
 
     private Date transactionDate;
 
+    //constructor
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
     }
-
+    
+    //method that retrieves the date of the transaction
+    public Date getDate() {
+        return transactionDate;
+    }
+    
+    //method that retrieves the amount of the transaction
+    public double getAmount() {
+        return amount;
+    }
 }
