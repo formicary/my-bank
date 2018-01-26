336bb992d7fd845b198e9c10a56b3264e29c54cc
Matt <m.howard4@lancaster.ac.uk>
2018-01-26 18:06:32 +0000

    -Renamed Bank.format() because the function name was not descriptive -Removed Bank.getFirstCustomer because it was poorly coded and unused. Unused getters are bad practice

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -33,14 +33,14 @@ public class Bank {
     public String customerSummary() {
         String summary = "Customer Summary";
         for (Customer c : customers) {
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
+            summary += "\n - " + c.getName() + " (" + pluraliseString(c.getNumberOfAccounts(), "account") + ")";
         }
         return summary;
     }
 
     //Make sure correct plural of word is created based on the number passed in:
     //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
+    private String pluraliseString(int number, String word) {
         //TODO: this is pretty janky
         return number + " " + (number == 1 ? word : word + "s");
     }
@@ -53,15 +53,5 @@ public class Bank {
         return total;
     }
 
-    public String getFirstCustomer() {
-        //TODO: wtf? This is weird
-        try {
-            customers = null;
-            return customers.get(0).getName();
-            //TODO: create explicit Exception type
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
-    }
+
 }
