dbd2806b56d4724ffb77b2b5ed84d2b8e511c159
Unknown <tunc@codesigned.co.uk>
2018-03-12 13:37:15 +0000

    Fixed all.

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -2,12 +2,10 @@ package com.abc;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-
 public class TransactionTest {
-    @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorFail(){
+        Transaction transaction = new Transaction(0, "t");
     }
 }
