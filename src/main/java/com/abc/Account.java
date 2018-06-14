59cff7ba539e544e8bc87fb5b5cb8b9c7baa7aae
dsortino <danny.sortino@ukaea.uk>
2018-06-15 00:52:30 +0100

    Fixed some minor formatting issues

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -25,13 +25,13 @@ public class Account {
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+	public void withdraw(double amount) {
+	    if (amount <= 0) {
+	        throw new IllegalArgumentException("amount must be greater than zero");
+	    } else {
+	        transactions.add(new Transaction(-amount));
+	    }
+	}
 
     public double interestEarned() {
         double amount = sumTransactions();
@@ -41,15 +41,14 @@ public void withdraw(double amount) {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
             case MAXI_SAVINGS:
-                if (amount <= 1000)
+                if (amount <= 1000) {
                     return amount * 0.02;
-                if (amount <= 2000)
+                }else if (amount <= 2000) {
                     return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+                }else {
+                	return 70 + (amount-2000) * 0.1;
+                }
             default:
                 return amount * 0.001;
         }
