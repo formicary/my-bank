c65a31bf1f2ac842ac0d20d3c241d10b32163c56
TristanSaunders <tristansaundersfr@gmail.com>
2018-07-27 19:47:11 +0200

    Change Maxi-Savings accounts to have an interest rate of 5% assuming no withdrawals in the past 10 days otherwise 0.1%.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,7 +1,9 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Account {
 
@@ -17,6 +19,10 @@ public class Account {
         this.transactions = new ArrayList<Transaction>();
     }
 
+    public int getAccountType() {
+        return accountType;
+    }
+
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
@@ -25,13 +31,13 @@ public class Account {
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        } else {
+            transactions.add(new Transaction(-amount));
+        }
     }
-}
 
     public double interestEarned() {
         double amount = sumTransactions();
@@ -41,33 +47,45 @@ public void withdraw(double amount) {
                     return amount * 0.001;
                 else
                     return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
+//            case MAXI_SAVINGS:
+//                if (amount <= 1000)
+//                    return amount * 0.02;
+//                if (amount <= 2000)
+//                    return 20 + (amount-1000) * 0.05;
+//                return 70 + (amount-2000) * 0.1;
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+                int lastWithdrawalIndex = 0;
+                for(int t = transactions.size()-1; t >= 0; t--){
+                    if (transactions.get(t).amount < 0){ // if transaction is a withdrawal
+                        lastWithdrawalIndex = t;
+                    }
+                }
+                Date now = DateProvider.getInstance().now();
+                Date lastTransaction = transactions.get(lastWithdrawalIndex).transactionDate;
+                // if there have been no withdrawals or more than 10 days since last withdrawal
+                if (lastWithdrawalIndex == 0 || getDaysSinceLastTransaction(lastTransaction, now, TimeUnit.DAYS) > 10)
+                    return amount * 0.05;
+                else
+                    return amount * 0.001;
             default:
                 return amount * 0.001;
         }
     }
 
     public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
+        if(transactions.size() == 0) { return 0.0; } //check if there have been transactions
         double amount = 0.0;
         for (Transaction t: transactions)
             amount += t.amount;
         return amount;
     }
 
-    public int getAccountType() {
-        return accountType;
+    public static long getDaysSinceLastTransaction(Date lastTrans, Date now, TimeUnit timeUnit){
+        long diffInDays = now.getTime() - lastTrans.getTime();
+        return timeUnit.convert(diffInDays,TimeUnit.MILLISECONDS);
     }
+//    public long getDaysSinceLastTransaction(Date lastTrans, Date now) {
+//        return (now.getTime() - lastTrans.getTime()) / (1000 * 60 * 60 * 24);
+//    }
 
 }
