66178eb6daadae69382dd925e6c5e799311ba169
Joseph Cooper <cooperjoe91@gmail.com>
2018-09-28 15:11:08 +0100

    Refactored code and added some necessary getters

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -1,5 +1,7 @@
 package com.abc;
 
+import static java.lang.Math.abs;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,20 +16,18 @@ public class Bank {
         customers.add(customer);
     }
 
-    public String customerSummary() {
-        String summary = "Customer Summary";
+    public String printCustomerAccountsReport() {
+        String report = "Customer Report";
         for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
+            report += "\n - " + c.getCustomerSummary();
+        return report;
+    }
+    
+    public String printTotalInterestPaidReport(){
+    	return "Total Interest Paid Report\n Total Interest Paid: " + toDollars(totalInterestPaid());
     }
 
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
-    }
-
-    public double totalInterestPaid() {
+    private double totalInterestPaid() {
         double total = 0;
         for(Customer c: customers)
             total += c.totalInterestEarned();
@@ -36,11 +36,13 @@ public class Bank {
 
     public String getFirstCustomer() {
         try {
-            customers = null;
             return customers.get(0).getName();
         } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
+        	throw new NullPointerException();
         }
+    }
+    
+    public static String toDollars(double d){
+        return String.format("$%,.2f", abs(d));
     }
 }
--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -3,8 +3,6 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Math.abs;
-
 public class Customer {
     private String name;
     private List<Account> accounts;
@@ -13,18 +11,66 @@ public class Customer {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
-
-    public String getName() {
-        return name;
-    }
-
+    
     public Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
 
+    public String getName() {
+        return name;
+    }
+    
     public int getNumberOfAccounts() {
         return accounts.size();
+    }
+    
+    public String getCustomerSummary() {
+    	return this.getName() + " (" + format(this.getNumberOfAccounts(), "account") + ")";
+    }
+
+    //Make sure correct plural of word is created based on the number passed in:
+    //If number passed in is 1 just return the word otherwise add an 's' at the end
+    private String format(int number, String word) {
+    	return number + " " + (number == 1 ? word : word + "s");
+    }
+      
+    public String getStatement() {
+        String statement = null;
+        statement = "Statement for " + name + "\n";
+        double total = 0.0;
+        for (Account a : accounts) {
+            statement += "\n" + statementForAccount(a) + "\n";
+            total += a.getBalance();
+        }
+        statement += "\nTotal In All Accounts " + Bank.toDollars(total);
+        return statement;
+    }
+    
+    private String statementForAccount(Account a) {
+        String s = "";
+
+       //Translate to account type string
+        switch(a.getAccountType()){
+            case CHECKING:
+                s += "Checking Account\n";
+                break;
+            case SAVINGS:
+                s += "Savings Account\n";
+                break;
+            case MAXI_SAVINGS:
+                s += "Maxi-Savings Account\n";
+                break;
+        }
+
+        //Now total up all the transactions
+        double total = 0.0;
+        for (Transaction t : a.getTransactions()) {
+            s += "  " + (t.getAmount() < 0 ? "withdrawal" : "deposit") + " " + Bank.toDollars(t.getAmount()) + " - " + (t.getTransactionDate()) + "\n";
+            total += t.getAmount();
+        }
+        s += "Total " + Bank.toDollars(total);
+        return s;
     }
 
     public double totalInterestEarned() {
@@ -34,45 +80,6 @@ public class Customer {
         return total;
     }
 
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
-    }
 
-    private String statementForAccount(Account a) {
-        String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
-
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
-        return s;
-    }
-
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
-    }
+    
 }
