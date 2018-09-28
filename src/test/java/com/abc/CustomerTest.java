04fd5958895d23679b31759d87a0f578f55b0ea9
Joseph Cooper <cooperjoe91@gmail.com>
2018-09-28 15:22:29 +0100

    Refactored CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -1,20 +1,22 @@
 package com.abc;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
+import java.time.format.DateTimeFormatter;
 
 public class CustomerTest {
 
     @Test //Test customer statement generation
     public void testApp(){
 
-        Account checkingAccount = new Account(Account.CHECKING);
-        Account savingsAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new Account(AccountType.CHECKING);
+        Account savingsAccount = new Account(AccountType.SAVINGS);
 
         Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
 
+        checkingAccount.deposit(100.0);
         checkingAccount.deposit(100.0);
         savingsAccount.deposit(4000.0);
         savingsAccount.withdraw(200.0);
@@ -22,36 +24,35 @@ public class CustomerTest {
         assertEquals("Statement for Henry\n" +
                 "\n" +
                 "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
+                "  deposit $100.00 - " + DateProvider.formattedDate(DateProvider.now()) + "\n" +
+                "  deposit $100.00 - " + DateProvider.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + "\n" +
+                "Total $200.00\n" +
                 "\n" +
                 "Savings Account\n" +
-                "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
+                "  deposit $4,000.00 - " + DateProvider.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + "\n" +
+                "  withdrawal $200.00 - " + DateProvider.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + "\n" +
                 "Total $3,800.00\n" +
                 "\n" +
-                "Total In All Accounts $3,900.00", henry.getStatement());
+                "Total In All Accounts $4,000.00", henry.getStatement());
     }
 
     @Test
     public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.SAVINGS));
+        Customer oscar = new Customer("Oscar").openAccount(new Account(AccountType.SAVINGS));
         assertEquals(1, oscar.getNumberOfAccounts());
     }
 
     @Test
     public void testTwoAccount(){
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+        Customer oscar = new Customer("Oscar").openAccount(new Account(AccountType.SAVINGS)).openAccount(new Account(AccountType.CHECKING));
         assertEquals(2, oscar.getNumberOfAccounts());
     }
 
-    @Ignore
+    @Test
     public void testThreeAcounts() {
         Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.SAVINGS));
-        oscar.openAccount(new Account(Account.CHECKING));
+                .openAccount(new Account(AccountType.SAVINGS));
+        oscar.openAccount(new Account(AccountType.CHECKING));
         assertEquals(3, oscar.getNumberOfAccounts());
     }
 }
