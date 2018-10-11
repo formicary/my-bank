c030baa849f02f55740ed90f21f6ddf731494ca4
George Price <georgepriiice@gmail.com>
2018-10-11 15:04:45 +0100

    Changing MAXI_SAVINGS interest calculation

--- src/main/java/com/abc/customer/Account.java
+++ src/main/java/com/abc/customer/Account.java
@@ -49,10 +49,7 @@ public class Account {
 //                    return 20;
 
             case MAXI_SAVINGS:
-                if (amount <= 1000)     return amount * 0.02;
-                if (amount <= 2000)     return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-
+                return amount * 0.01;
             default:                    return amount * 0.001;
         }
     }
