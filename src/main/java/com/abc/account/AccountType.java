81e67af83378c21dd3ec2269335745a111094285
George Price <georgepriiice@gmail.com>
2018-10-11 14:09:57 +0100

    AccountType commenting

--- src/main/java/com/abc/account/AccountType.java
+++ src/main/java/com/abc/account/AccountType.java
@@ -1,5 +1,8 @@
 package com.abc.account;
 
+/**
+ * Represents a unique account type, used to determine interest earned for a given Account
+ */
 public enum AccountType {
 
     CHECKING, SAVINGS, MAXI_SAVINGS;
