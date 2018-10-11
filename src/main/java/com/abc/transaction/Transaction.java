ac6a4cdf285bbfb9fdf2d93214950c1f2a744813
George Price <georgepriiice@gmail.com>
2018-10-11 14:25:00 +0100

    Added Transaction class into its own package, refactored.

---
+++ src/main/java/com/abc/transaction/Transaction.java
@@ -0,0 +1,29 @@
+package com.abc.transaction;
+
+import java.util.Date;
+
+import com.abc.helper.DateProvider;
+import com.abc.helper.Strings;
+
+/**
+ * Stores a transaction for a given amount on a given date
+ */
+public class Transaction {
+
+    public final double amount; // the transaction amount, in dollars
+    private Date date;          // the date when the transaction took place
+
+    /**
+     * Constructor, requires the dollar amount involved
+     * @param amount double representing the dollar amount of the transaction
+     */
+    public Transaction(double amount) {
+        this.amount = amount;
+        this.date = DateProvider.now();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", Strings.toDollars(amount), amount < 0 ? "withdrawal" : "deposit");
+    }
+}
