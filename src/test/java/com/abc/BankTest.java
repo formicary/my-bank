7c69f153e366e38db5ed2bd7e6fb4d64f0fd79f3
KevinDervi <k1502436@kcl.ac.uk>
2018-10-17 22:55:17 +0100

    updated bank tests

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -2,6 +2,7 @@ package com.abc;
 
 import com.abc.Accounts.Account;
 import com.abc.Accounts.CheckingAccount;
+import com.abc.Accounts.SavingsAccount;
 import com.abc.util.Money;
 import org.junit.Test;
 
@@ -12,6 +13,7 @@ import static org.junit.Assert.assertEquals;
 public class BankTest {
     private static final Money DELTA = new Money("1e-15");
 
+    // TODO add extra customer summary tests
     @Test
     public void customerSummary() {
         Bank bank = new Bank();
@@ -22,6 +24,7 @@ public class BankTest {
         assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
     }
 
+    // TODO add extra checking account tests for edge cases
     @Test
     public void checkingAccount() {
         Bank bank = new Bank();
@@ -43,17 +46,31 @@ public class BankTest {
         assert(lessThanDelta);
     }
 
-//    @Test
-//    public void savings_account() {
-//        Bank bank = new Bank();
-//        Account checkingAccount = new SavingsAccount();
-//        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
-//
-//        checkingAccount.deposit(1500.0);
-//
-//        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-//    }
-//
+
+    // TODO add extra savings account tests for edge cases
+
+    @Test
+    public void savings_account() {
+        Bank bank = new Bank();
+        Account checkingAccount = new SavingsAccount();
+        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+
+        checkingAccount.deposit(new Money("1500"));
+
+        // value we are aiming to get
+        Money targetValue = new Money("2.0");
+
+        // absolute difference between target value and test value
+        Money difference = targetValue.subtract(bank.totalInterestPaid()).abs();
+
+        // if difference between test and target is less than delta then it is considered accurate
+        boolean lessThanDelta = DELTA.compareTo(difference) >= 0;
+
+        assert(lessThanDelta);
+    }
+
+    // TODO add extra maxi savings account tests for edge cases
+
 //    @Test
 //    public void maxi_savings_account() {
 //        Bank bank = new Bank();
