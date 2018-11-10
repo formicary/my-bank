1638de3abfd5491e4a723e84803126fa060437cc
MS <email@example.com>
2018-11-10 21:53:56 +0000

    Simplified test for totalInterestPaid
    Removed individual tests.
    Accounts now set up before all tests.
    Simple tests added to ensure interest calculated is correct.

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -1,5 +1,6 @@
 package com.abc;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,73 +8,49 @@ import static org.junit.Assert.assertEquals;
 public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
-    @Test
-    public void customerSummary() {
-        Bank bank = new Bank();
+    private static Bank bank;
+
+    @BeforeClass
+    public static void setup() {
+        bank = new Bank();
         Customer john = new Customer("John");
         john.openAccount(new Account(AccountTypes.CHECKING));
+        john.getAccounts().get(0).deposit(800);
         bank.addCustomer(john);
 
         Customer henry = new Customer("Henry");
         henry.openAccount(new Account(AccountTypes.CHECKING));
+        henry.getAccounts().get(0).deposit(1000);
         henry.openAccount(new Account(AccountTypes.SAVINGS));
+        henry.getAccounts().get(1).deposit(1500);
         bank.addCustomer(henry);
 
         Customer oscar = new Customer("Oscar");
         oscar.openAccount(new Account(AccountTypes.CHECKING));
+        oscar.getAccounts().get(0).deposit(1200);
         oscar.openAccount(new Account(AccountTypes.SAVINGS));
+        oscar.getAccounts().get(1).deposit(1800);
         oscar.openAccount(new Account(AccountTypes.MAXI_SAVINGS));
+        oscar.getAccounts().get(2).deposit(2500);
         bank.addCustomer(oscar);
+    }
 
+    @Test
+    public void customerSummary() {
         String expected = "Customer Summary";
         for (Customer c : bank.getCustomers()) {
             expected += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
         }
-        
+
         assertEquals(expected, bank.customerSummary());
+    }
+
+    @Test
+    public void totalInterestPaid() {
+        assertEquals(127.6, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
     private String format(int number, String word) {
         return number + " " + (number == 1 ? word : word + "s");
     }
-
-    @Test
-    public void checkingAccount() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(AccountTypes.CHECKING);
-        Customer bill = new Customer("Bill");
-        bill.openAccount(checkingAccount);
-        bank.addCustomer(bill);
-
-        checkingAccount.deposit(100.0);
-
-        assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
-    @Test
-    public void savings_account() {
-        Bank bank = new Bank();
-        Account savingAccount = new Account(AccountTypes.SAVINGS);
-        Customer bill = new Customer("Bill");
-        bank.addCustomer(bill);
-        bill.openAccount(savingAccount);
-
-        savingAccount.deposit(1500.0);
-
-        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
-    @Test
-    public void maxi_savings_account() {
-        Bank bank = new Bank();
-        Account maxiSavingAccount = new Account(AccountTypes.MAXI_SAVINGS);
-        Customer bill = new Customer("Bill");
-        bank.addCustomer(bill);
-        bill.openAccount(maxiSavingAccount);
-
-        maxiSavingAccount.deposit(3000.0);
-
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
-
 }
