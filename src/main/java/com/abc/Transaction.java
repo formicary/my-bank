b170197ab91e3884d195ed344a11d8da4fc5fc78
MS <email@example.com>
2018-11-10 17:40:58 +0000

    Added getters()

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -6,11 +6,18 @@ import java.util.Date;
 public class Transaction {
     public final double amount;
 
-    private Date transactionDate;
+    public Date transactionDate;
 
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
     }
 
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
