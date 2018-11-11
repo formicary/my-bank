aa563ef818f1f9ca7f02a7dd533ac2f2f0b2e54b
MS <email@example.com>
2018-11-11 13:26:37 +0000

    Test implemented for modified interest for MAXI_SAVINGS
    Checks date of last transaction and returns correct interest.
    Code for setting last withdrawl date for testing commented out.

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -1,8 +1,11 @@
 package com.abc;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -51,5 +54,40 @@ public class AccountTest {
         assertEquals(45, maxi.interestEarned(), DOUBLE_DELTA);
         maxi.deposit(1000);
         assertEquals(120, maxi.interestEarned(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void checkLastTransaction() {
+        double interest = 0;
+        Account account = new Account(AccountTypes.MAXI_SAVINGS);
+
+        account.deposit(5000);
+        account.withdraw(700);
+        account.withdraw(700);
+        account.deposit(1700);
+
+        Date today = DateProvider.getInstance().now();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(today);
+        cal.add(Calendar.DAY_OF_YEAR, -10);
+        Date tenDays = cal.getTime();
+
+        List<Transaction> transactions = account.getTransactions();
+        Transaction last = null;
+        for (int i = transactions.size() - 1; i >= 0; i-- ) {
+            if (transactions.get(i).getAmount() < 0) {
+                last = transactions.get(i);
+                break;
+            }
+        }
+//        cal.add(Calendar.DAY_OF_YEAR, -5);
+//        last.setTransactionDate(cal.getTime());
+        if (last.getTransactionDate().before(tenDays) ) {
+            interest = 0.05;
+        } else if (last.getTransactionDate().after(tenDays)) {
+            interest = 0.001;
+        }
+        assertEquals(0.001, interest, DOUBLE_DELTA);
+//        assertEquals(0.05, interest, DOUBLE_DELTA);
     }
 }
