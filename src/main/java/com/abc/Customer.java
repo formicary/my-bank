7bd5373f2960c802006534b8ac12b4f558884f53
Sakariye Mahdi <saka12@hotmail.co.uk>
2019-01-31 17:35:18 +0000

    openAccount is now void.

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -18,9 +18,8 @@ public class Customer {
         return name;
     }
 
-    public Customer openAccount(Account account) {
+    public void openAccount(Account account) {
         accounts.add(account);
-        return this;
     }
 
     public int getNumberOfAccounts() {
