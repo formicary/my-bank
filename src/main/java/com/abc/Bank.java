822dfac55d2ffcd8c3b84e1e6c25453e194b3c21
Sakariye Mahdi <saka12@hotmail.co.uk>
2019-01-31 18:59:18 +0000

    Removed getFirstCustomer as it was never used.

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -34,12 +34,5 @@ public class Bank {
         return total;
     }
 
-    public String getFirstCustomer() {
-        try {
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
     }
-}
+
