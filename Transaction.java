58cc2ff6394635b1c8efe8e26c10e3bffc0826bc
DSey <47179803+DSey@users.noreply.github.com>
2019-02-01 11:28:04 +0000

    Add files via upload
    Name: Daniel Seymour
    Phone: +447585228414
    Email: danseymour1993@outlook.com
    The recruiter told me I had 48 hours to complete this test, hence due to conflicting work I did not have time to make all the changes I wanted to make. I hate leaving things unfinished so I will make another pull request with the finished work.

---
+++ Transaction.java
@@ -0,0 +1,16 @@
+package com.abc;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Transaction {
+    public final double amount;
+
+    private Date transactionDate;
+
+    public Transaction(double amount) {
+        this.amount = amount;
+        this.transactionDate = DateProvider.getInstance().now();
+    }
+
+}
