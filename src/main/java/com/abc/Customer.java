48999f4b472f6e9a5f128438c429a3ce7fd129a0
DSey <47179803+DSey@users.noreply.github.com>
2019-02-01 11:27:07 +0000

    Add files via upload
    Name: Daniel Seymour
    Phone: +447585228414
    Email: danseymour1993@outlook.com
    The recruiter told me I had 48 hours to complete this test, hence due to conflicting work I did not have time to make all the changes I wanted to make. I hate leaving things unfinished so I will make another pull request with the finished work.

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,5 +1,11 @@
 package com.abc;
 
+/**
+ * Class which represents the customers of the bank.
+ * 
+ * @author Daniel Seymour
+ * @version 1.0
+ */
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,24 +15,82 @@ public class Customer {
     private String name;
     private List<Account> accounts;
 
+    /**
+     * Constructs a List of Account objects with a dynamic type of an ArrayList of 
+     * Account objects. Represents the bank account held by the customer.
+     * 
+     * @param name String representation of the customer's name.
+     */
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
+    /**
+     * Accessor for the name of the customer for whom its called.
+     * @return String representation of the customer's name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Adds an object of type Account to the List instantiated in the constructor.
+     * @param account The account to be added to the list.
+     * @return The list containing the newly added account.
+     */
     public Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
 
+    /**
+     * Accessor for the number of accounts in the List which calls it.
+     * @return The size of the list that calls it.
+     */
     public int getNumberOfAccounts() {
         return accounts.size();
     }
+    
+    /**
+     * This method can be called to transfer a specified amount from one account to another.
+     * @param amount The sum to be transferred.
+     * @param accountFrom The account to transfer the money from.
+     * @param accountTo The account to transfer the money to.
+     */
+    public void transferSum(double amount, Account accountFrom, Account accountTo) {
+    	if(!checkIfAccountsExist(accountFrom, accountTo)) { // calls this method to check that the customer owns the types of accounts needed.
+    		throw new IllegalArgumentException("Must own both account types");
+    	}
+    	else {
+    		accountFrom.withdraw(amount); // withdraws the amount argument from the first account.
+    		accountTo.deposit(amount); // deposits the amount argument in the second account.
+    	}
+    }
+    
+    /**
+     * Checks if the customer who calls the method owns the accounts supplied as arguments.
+     * @param accountA Account of a certain subtype.
+     * @param accountB Account of another subtype.
+     * @return boolean representing whether this is the case.
+     */
+    public boolean checkIfAccountsExist(Account accountA, Account accountB) {
+    	for (Account a : this.accounts) {
+    		if (a.getAccountType() == accountA.getAccountType()) {
+    			for (Account b : this.accounts) {
+    				if (b.getAccountType() == accountB.getAccountType()) {
+    					return true;
+    				}
+    			}
+    		}
+    	}
+    	return false;
+    }
 
+    /**
+     * Scans each account owned by the customer object that calls it and sums their interest.
+     * @return double representing the sum of the interest accrued by each account.
+     */
     public double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts)
@@ -34,6 +98,10 @@ public class Customer {
         return total;
     }
 
+    /**
+     * Concatenates String representations of of each account the customer has.
+     * @return String representation of the customer's statement detailing each account.
+     */
     public String getStatement() {
         String statement = null;
         statement = "Statement for " + name + "\n";
@@ -46,6 +114,12 @@ public class Customer {
         return statement;
     }
 
+    /**
+     * Converts the integer representing the account type into a String.
+     * 
+     * @param a The account to be converted.
+     * @return String representation of the account's type.
+     */
     private String statementForAccount(Account a) {
         String s = "";
 
@@ -72,6 +146,11 @@ public class Customer {
         return s;
     }
 
+    /**
+     * Converts a double into a String representation to two decimal places preceded by a dollar sign.
+     * @param d The double to be converted.
+     * @return String representation of the double as dollars and cents.
+     */
     private String toDollars(double d){
         return String.format("$%,.2f", abs(d));
     }
