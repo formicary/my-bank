259520d4d9269de7cb07b2b09024760fc612ce4e
add754 <43729876+add754@users.noreply.github.com>
2019-02-03 20:15:51 +0000

    Update BankTest.java

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -29,26 +29,64 @@ public class BankTest {
         assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
-    @Test
+    @Test // edited test names to reflect account being tested
     public void savings_account() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+        Account savingsAccount = new Account(Account.SAVINGS);
+        bank.addCustomer(new Customer("Bill").openAccount(savingsAccount));
 
-        checkingAccount.deposit(1500.0);
+        savingsAccount.deposit(1500.0);
 
         assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
-    @Test
+    @Test // edited test names to reflect account being tested
+    // edited interest to be paid based on edited method in Account class
     public void maxi_savings_account() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+        Account maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
+        bank.addCustomer(new Customer("Bill").openAccount(maxiSavingsAccount));
 
-        checkingAccount.deposit(3000.0);
+        maxiSavingsAccount.deposit(3000.0);
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        assertEquals(150.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
+    // New test of Super Savings account
+
+    @Test
+    public void super_savings_account() {
+        Bank bank = new Bank();
+        Account superSavingsAccount = new Account(Account.SUPER_SAVINGS);
+        bank.addCustomer(new Customer("Bill").openAccount(superSavingsAccount));
+
+        superSavingsAccount.deposit(3000.0);
+
+        assertEquals(3020.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    // test getFirstCustomer() method
+    @Test
+    public void get_first_customer() {
+        Bank bank = new Bank();
+        Customer john = new Customer("John");
+        Customer bill = new Customer("Bill");
+        Customer diane = new Customer("Diane");
+        john.openAccount(new Account(Account.CHECKING));
+        bill.openAccount(new Account(Account.MAXI_SAVINGS));
+        diane.openAccount(new Account(Account.SUPER_SAVINGS));
+        bank.addCustomer(john);
+        bank.addCustomer(bill);
+        bank.addCustomer(diane);
+
+        assertEquals("John", bank.getFirstCustomer());
+    }
+
+    // test getFirstCustomer method for exception
+    @Test
+    public void get_first_customer_Exception() throws Exception {
+        Bank bank = new Bank();
+
+        bank.getFirstCustomer();
+    }
 }
