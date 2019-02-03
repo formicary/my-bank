d970422a796786944a22e5eecef207ee1aad2f8c
add754 <43729876+add754@users.noreply.github.com>
2019-02-03 20:15:14 +0000

    Update Transaction.java
    by Aaron Dougherty

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,20 @@
 package com.abc;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate; // changed all methods to implement new up to data Java Date class
 
 public class Transaction {
     public final double amount;
 
-    private Date transactionDate;
+    private LocalDate transactionDate;
 
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
+    }
+
+    // getter for transaction date
+    public LocalDate getTransactionDate() {
+        return transactionDate;
     }
 
 }
