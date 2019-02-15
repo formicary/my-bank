40f708989a37d553b2e6576a7a878723da9d6f3d
channicky96 <47661309+channicky96@users.noreply.github.com>
2019-02-15 19:15:18 +0000

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,26 @@
 package com.abc;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Transaction {
+
+    // Fields representing a transaction
     public final double amount;
+    private final LocalDate transactionDate;
 
-    private Date transactionDate;
-
+    /**
+     *
+     * Creates a new instance of a transaction
+     *
+     * @param amount the amount of money of the transaction
+     */
     public Transaction(double amount) {
         this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+        this.transactionDate = LocalDate.now();
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
     }
 
 }
