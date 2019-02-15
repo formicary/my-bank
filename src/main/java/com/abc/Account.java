bd1a2fa5170058847d0a7adb785830cc6ccfeb1f
channicky96 <47661309+channicky96@users.noreply.github.com>
2019-02-15 19:12:47 +0000

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,22 +1,37 @@
 package com.abc;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
 
+    //Fields to represent an account
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
 
     private final int accountType;
+    private final double balance;
     public List<Transaction> transactions;
 
+    /**
+     * Creates a new instance of an account
+     *
+     * @param accountType the account type of the customer
+     *
+     */
     public Account(int accountType) {
         this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
     }
 
+    /**
+     *
+     * @param amount The amount of money the customer wish to deposit to this
+     * account
+     */
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
@@ -25,49 +40,82 @@ public class Account {
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
-
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
+    /**
+     *
+     * @param amount The amount of money the customer wish to withdraw from this
+     * account
+     */
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        } else {
+            if (amount >= balance) {
+                throw new IllegalArgumentException("you do not have enough balance in account");
+            } else {
+                transactions.add(new Transaction(-amount));
+            }
         }
     }
 
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
+    /**
+     *
+     * @return The amount of interest earned providing the customer's account
+     * type
+     */
+    public double interestEarned() {
+        double amount = sumTransactions();
+        if (amount >= 0.0) {
+            switch (accountType) {
+                case CHECKING:
+                    return amount * 0.001;
+                case SAVINGS:
+                    if (amount <= 1000) {
+                        return amount * 0.001;
+                    } else {
+                        return 1 + (amount - 1000) * 0.002;
+                    }
+                case MAXI_SAVINGS:
+                    if (transactions.size() > 2) {
+                        LocalDate lastTransactionDate = transactions.get(transactions.size() - 1).getTransactionDate();
+                        LocalDate secoundToLastTransactionDate = transactions.get(transactions.size() - 2).getTransactionDate();
+                        if (lastTransactionDate.getDayOfYear() >= secoundToLastTransactionDate.getDayOfYear()
+                                && lastTransactionDate.getMonthValue() >= secoundToLastTransactionDate.getMonthValue()
+                                && lastTransactionDate.getDayOfMonth() < 10 + secoundToLastTransactionDate.getDayOfMonth()) {
+                            return amount * 0.05;
+                        }
+                    }
+                default:
+                    return amount * 0.001;
+            }
+        }
+        return 0;
     }
 
-    private double checkIfTransactionsExist(boolean checkAll) {
+    /**
+     *
+     * @return The sum of transactions of an account
+     */
+    public double sumTransactions() {
         double amount = 0.0;
-        for (Transaction t: transactions)
+        for (Transaction t : transactions) {
             amount += t.amount;
+        }
         return amount;
     }
 
+    /**
+     *
+     * @return The account type of this account
+     */
     public int getAccountType() {
         return accountType;
     }
 
+    /**
+     *
+     * @return The account balance
+     */
+    public double getBalance() {
+        return balance;
+    }
 }
