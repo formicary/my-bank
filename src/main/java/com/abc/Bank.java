b5ca079724c77c5d5c36880af3bae26e746bd203
Marco Furone <marco.job92@gmail.com>
2019-02-20 11:10:40 +0000

    Update Bank.java

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map.Entry;
 
 public class Bank {
-	private String name;
+    private String name;
     private List<Customer> customers = new ArrayList<Customer>();
     
     public Bank(String name) {
