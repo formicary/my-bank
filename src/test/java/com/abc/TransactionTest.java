1d78a07c9620115331e2c3dce60773569cca26ed
Marco Furone <marco.job92@gmail.com>
2019-02-20 11:19:26 +0000

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,32 @@
 package com.abc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import consts.Constants;
+
 
 public class TransactionTest {
+	
     @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
+    public void testCreationgOfTransactions() {
+    	Transaction d, w;
+    	try {
+    		 d = new Transaction(-50.40, Constants.DEPOSIT);
+    		fail();
+    	} catch(IllegalArgumentException e) { }
+    	d = new Transaction(50.40, Constants.DEPOSIT);
+        assertEquals(50.40, d.getAmount(), 0);
+        assertEquals(Constants.DEPOSIT, d.getTransactionType());
+        try {
+        	w = new Transaction(49, Constants.WITHDRAWAL);
+        	fail();
+        } catch(IllegalArgumentException e) { }
+        w = new Transaction(-49, Constants.WITHDRAWAL);
+        assertEquals(-49, w.getAmount(), 0);
+        assertEquals(Constants.WITHDRAWAL, w.getTransactionType());
     }
+    
 }
