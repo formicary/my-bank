c97b4aad3e6babcf46029e0044910c887bc5098a
Chris Leach <chris@leach.sh>
2019-02-27 12:21:00 +0000

    Add customerSummary tests.

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -18,6 +18,32 @@ public class BankTest {
     }
 
     @Test
+    public void customerSummaryMultipleAccounts() {
+        Bank bank = new Bank();
+        Customer john = new Customer("John");
+        john.openAccount(new Account(Account.CHECKING));
+        john.openAccount(new Account(Account.SAVINGS));
+        john.openAccount(new Account(Account.MAXI_SAVINGS));
+        bank.addCustomer(john);
+
+        assertEquals("Customer Summary\n - John (3 accounts)", bank.customerSummary());
+    }
+
+    @Test
+    public void customerSummaryMultipleCustomers() {
+        Bank bank = new Bank();
+        Customer john = new Customer("John");
+        john.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(john);
+        Customer jane = new Customer("Jane");
+        jane.openAccount(new Account(Account.SAVINGS));
+        jane.openAccount(new Account(Account.MAXI_SAVINGS));
+        bank.addCustomer(jane);
+
+        assertEquals("Customer Summary\n - John (1 account)\n - Jane (2 accounts)", bank.customerSummary());
+    }
+
+    @Test
     public void checkingAccount() {
         Bank bank = new Bank();
         Account checkingAccount = new Account(Account.CHECKING);
