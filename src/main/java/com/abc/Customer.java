2a33e7743cea2f0d6406a756fd33fb620353444c
vincent Yeadon <v.yeadon@hotmail.co.uk>
2019-03-07 10:35:54 +0000

    Added final documentation and cahgned method names

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -5,28 +5,59 @@ import static java.lang.Math.abs;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Class Customer.
+ */
 public class Customer {
     private String name;
     private List<Account> accounts;
 
+    /**
+     * Instantiates a new customer.
+     *
+     * @param name the customers name
+     */
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
+    /**
+     * Gets the customers name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * Gets the customers accounts.
+     *
+     * @return the accounts
+     */
     public List<Account> getAccounts() {
 		return accounts;
 	}
 
+	/**
+	 * Opens an account of specified account type.
+	 *
+	 * @param account the account to be opened
+	 * @return the customer
+	 */
 	public Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
 	
+	/**
+	 * Transfer funds between two of the customers account.
+	 *
+	 * @param accountTransferFrom the account to transfer from
+	 * @param accountTransferTo the account to transfer to
+	 * @param amount the amount to transfer
+	 */
 	public void transferFunds(Account accountTransferFrom, Account accountTransferTo, double amount) {
 		if(accountTransferFrom.equals(accountTransferTo)) {
 			throw new IllegalArgumentException("Cannot transfer funds between the same account"); 
@@ -36,10 +67,20 @@ public class Customer {
 		accountTransferTo.deposit(amount);
 	}
 
+    /**
+     * Gets the number of accounts the customer has.
+     *
+     * @return the number of accounts
+     */
     public int getNumberOfAccounts() {
         return accounts.size();
     }
 
+    /**
+     * Total interest earned over all accounts.
+     *
+     * @return the interest
+     */
     public double totalInterestEarned() {
         double total = 0;
         for (Account account : accounts)
@@ -47,22 +88,27 @@ public class Customer {
         return total;
     }
 
+    /**
+     * Gets the customers overview statement.
+     * Contains information for all accounts.
+     *
+     * @return the statement
+     */
     public String getStatement() {
         String statement = null;
         statement = "Statement for " + name + "\n";
         double total = 0.0;
         for (Account account : accounts) {
-            statement += "\n" + statementForAccount(account) + "\n";
+            statement += "\n" + formatAccountStringForStatement(account) + "\n";
             total += account.sumTransactions();
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
+        statement += "\nTotal In All Accounts " + doubleToDollars(total);
         return statement;
     }
 
-    private String statementForAccount(Account account) {
+    private String formatAccountStringForStatement(Account account) {
         String s = "";
 
-       //Translate to pretty account type
         switch(account.getAccountType()){
             case Account.CHECKING:
                 s += "Checking Account\n";
@@ -75,17 +121,16 @@ public class Customer {
                 break;
         }
 
-        //Now total up all the transactions
         double total = 0.0;
         for (Transaction transaction : account.transactions) {
-            s += "  " + (transaction.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(transaction.amount) + "\n";
+            s += "  " + (transaction.amount < 0 ? "withdrawal" : "deposit") + " " + doubleToDollars(transaction.amount) + "\n";
             total += transaction.amount;
         }
-        s += "Total " + toDollars(total);
+        s += "Total " + doubleToDollars(total);
         return s;
     }
 
-    private String toDollars(double d){
+    private String doubleToDollars(double d){
         return String.format("$%,.2f", abs(d));
     }
 }
--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -4,17 +4,27 @@ import java.util.Date;
 
 import com.google.common.annotations.VisibleForTesting;
 
+/**
+ * The Class Transaction.
+ */
 public class Transaction {
 	
+    /** The transaction amount. */
     public final double amount;
 
     private Date transactionDate;
 
+    /**
+     * Instantiates a new transaction with date at which the transaction was performed.
+     *
+     * @param amount the transaction amount
+     */
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
     }
     
+
     @VisibleForTesting
     protected Transaction(double amount, Date transactionDate) {
     	 this.amount = amount;
