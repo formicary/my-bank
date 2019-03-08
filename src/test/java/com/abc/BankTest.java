dfbe7a405c1b857d83f3484842d22f2188c178b7
StefLianos <stef.lianos@gmail.com>
2019-03-08 16:54:26 +0000

    Completed Accenture Test

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.Date;
+import java.text.DateFormat;
+
 public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
@@ -48,7 +51,37 @@ public class BankTest {
 
         checkingAccount.deposit(3000.0);
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        //old maxi_savings
+        //assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        assertEquals(3, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void maxi_savings_interest(){
+
+        Bank bank = new Bank();
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        bank.addCustomer(new Customer("Chungus").openAccount(checkingAccount));
+        checkingAccount.deposit(100);
+
+        //get present time
+        DateProvider dp = new DateProvider();
+        long t = dp.now().getTime();
+
+        //construct past dates
+        Date dateMinus5 = new Date(dp.now().getTime()-(5*24*60*60*1000));
+        Date dateMinus15 = new Date(dp.now().getTime()-(15*24*60*60*1000));
+
+        //set lastWithdraw to 5 days
+        checkingAccount.setLastWithdraw(dateMinus5);
+
+        assertEquals(0.1, bank.totalInterestPaid(),DOUBLE_DELTA);
+
+        //set lastWithdraw to 15 days
+        checkingAccount.setLastWithdraw(dateMinus15);
+
+        assertEquals(0.5, bank.totalInterestPaid(),DOUBLE_DELTA);
+
     }
 
 }
