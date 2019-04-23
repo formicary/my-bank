c386a6ad9ef1fefedb55e72c7bf096e54093d2b4
Fearghus Oman <foman01@qub.ac.uk>
2019-04-23 17:26:06 +0100

    Commit of src files

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,73 +1,154 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.time.temporal.ChronoUnit;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
+/**
+ * 
+ * @author fearg
+ *
+ */
 public class Account {
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
-
-    private final int accountType;
+    private final AccountType accountType;
     public List<Transaction> transactions;
 
-    public Account(int accountType) {
+    /**
+     * constructor with accountType as argument
+     * account transactions are set to a new empty ArrayList of type Transactions
+     * @param accountType
+     */
+    public Account(AccountType accountType) {
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
     }
 
+    /**
+     * once the attempted transaction is above 0 then it is added to the arraylist
+     * @param amount
+     */
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
-        } else {
+        } 
+        else {
             transactions.add(new Transaction(amount));
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+    /**
+     * once the attempted transaction is above 0 then it is added to the arraylist
+     * there is no check to see whether the customer can afford the transaction
+     * @param amount
+     */
+	public void withdraw(double amount) {
+		
+	    if (amount <= 0) {
+	        throw new IllegalArgumentException("amount must be greater than zero");
+	    } 
+	    else {
+	        transactions.add(new Transaction(-amount));
+	    }
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
     public double interestEarned() {
+    	
         double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
+        
+        //only calculate interest if the account is in positive balance
+        if(amount > 0) {
+        	switch(accountType){
+            	case SAVINGS:
+	                if (amount <= 1000) {
+	                    return amount * 0.001;
+	                }
+	                else {
+	                    return (1000 * 0.001) + ((amount-1000) * 0.002);
+	                }
+	                
+	            case MAXI_SAVINGS:
+	            	Date mostRecentTransaction;
+	            	if(!transactions.isEmpty()) {
+	            		mostRecentTransaction = transactions.get(transactions.size() - 1).getTransactionDate();
+	            	}
+	            	else {
+	            		mostRecentTransaction = null;
+	            	}
+	            	
+	            	Date today = new Date();
+	            	
+	            	LocalDate d1 = mostRecentTransaction.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	            	LocalDate d2 = today.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	            	Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
+	            	long diffDays = diff.toDays();
+	            	
+	            	//if its been more than 10 days since the last transaction then apply 5% rate
+	            	if(diffDays > 10) {
+	            		return amount * 0.05;
+	            	}
+	            	else {
+	            		return amount * 0.001;
+	            	}
+	            
+	            //default is a checking account
+	            default:
+	                return amount * 0.001;
+	        }
         }
+        //otherwise, no interest accrued
+        else {
+        	return 0;
+        }
+        
     }
 
+    /**
+     * if there are transactions on the account then sum the amounts
+     * @return
+     */
     public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
+    	boolean transactionsExist = checkIfTransactionsExist();
+    	double amount = 0.0;
+    	
+    	if(transactionsExist == true) {
+    		for (Transaction t: transactions) {
+                amount += t.amount;
+    		}
+    	}
+   
         return amount;
     }
 
-    public int getAccountType() {
-        return accountType;
+    /**
+     * if there are transactions on the account then return true
+     * @return
+     */
+    private boolean checkIfTransactionsExist() {
+        boolean transactionsExist = false;
+        
+        if(transactions.isEmpty() == false) {
+        	transactionsExist = true;
+        }
+        
+        return transactionsExist;
     }
 
+    /**
+     * getter for accountType
+     * @return
+     */
+    public AccountType getAccountType() {
+        return accountType;
+    }
 }
--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -3,41 +3,85 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Math.abs;
-
+/**
+ * 
+ * @author fearg
+ *
+ */
 public class Customer {
-    private String name;
+
+	private String name;
     private List<Account> accounts;
 
+    /**
+     * constructor that takes name as arg
+     * @param name
+     */
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
+    /**
+     * getter for name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
-    public Customer openAccount(Account account) {
+   /**
+    * open account takes an account as an arg and adds it to customer's list
+    * @param account
+    */
+    public void openAccount(Account account) {
         accounts.add(account);
-        return this;
     }
-
+    
+    /**
+     * returns number of accounts belonging to the customer
+     * @return
+     */
     public int getNumberOfAccounts() {
         return accounts.size();
     }
+    
+    /**
+     * if customer does own both accounts then the speicifed amount will be withdrawn from one
+     * and deposited in the other
+     * @param accountFrom
+     * @param accountTo
+     * @param amount
+     */
+    public void transferBetweenAccounts(Account accountFrom, Account accountTo, double amount) {
+    	if(accounts.contains(accountFrom) && accounts.contains(accountTo)) {
+    		accountFrom.withdraw(amount);
+    		accountTo.deposit(amount);
+    	}
+    }
 
+    /**
+     * adds interest from all accounts
+     * @return
+     */
     public double totalInterestEarned() {
         double total = 0;
-        for (Account a : accounts)
+        
+        for (Account a : accounts) {
             total += a.interestEarned();
+        }
         return total;
     }
 
+    /**
+     * returns a string that can be interpreted as the customer's statement
+     * @return
+     */
     public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
+    	
+        String statement = "Statement for " + name + "\n";
         double total = 0.0;
+        
         for (Account a : accounts) {
             statement += "\n" + statementForAccount(a) + "\n";
             total += a.sumTransactions();
@@ -46,33 +90,45 @@ public class Customer {
         return statement;
     }
 
+    /**
+     * 
+     * @param a
+     * @return
+     */
     private String statementForAccount(Account a) {
         String s = "";
+        double running_total = 0.0;
 
        //Translate to pretty account type
         switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
+            case SAVINGS:
                 s += "Savings Account\n";
                 break;
-            case Account.MAXI_SAVINGS:
+            case MAXI_SAVINGS:
                 s += "Maxi Savings Account\n";
+                break;
+            default:
+            	s += "Checking Account\n";
                 break;
         }
 
         //Now total up all the transactions
-        double total = 0.0;
         for (Transaction t : a.transactions) {
             s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
+            running_total += t.amount;
         }
-        s += "Total " + toDollars(total);
+        
+        s += "Total " + toDollars(running_total);
         return s;
     }
 
+    /**
+     * convert the double to dollar format
+     * @param d
+     * @return
+     */
     private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
+        return String.format("$%,.2f", Math.abs(d));
     }
+    
 }
