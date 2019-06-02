bfb199cddd572c620e5e53c08f70040c9e634e3e
Sean Tomlinson <sean.wtomlinson@talktalk.net>
2019-06-02 20:31:49 +0100

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,29 @@
 package com.abc;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Transaction {
-    public final double amount;
+	
+    private final double amount;
 
-    private Date transactionDate;
+    private LocalDate transactionDate;
 
     public Transaction(double amount) {
         this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+        this.transactionDate = LocalDate.now();
+    }
+    
+    public Transaction(double amount, LocalDate date) {
+        this.amount = amount;
+        this.transactionDate = date;
     }
 
+	public double getAmount() {
+		return amount;
+	}
+
+	public LocalDate getTransactionDate() {
+		return transactionDate;
+	}
+ 
 }
