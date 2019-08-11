ab90a905c58cfc809b37e6565efdb508d925fe62
Arjun <arjunlotay@hotmail.com>
2019-08-11 13:15:17 +0100

    Tranfer

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -43,4 +43,5 @@ public class Bank {
             return "Error";
         }
     }
+    
 }
--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -75,4 +75,9 @@ public class Customer {
     private String toDollars(double d){
         return String.format("$%,.2f", abs(d));
     }
+    
+    public void transfer(Account from, Account to, double amount) {
+    	from.withdraw(amount);
+    	to.deposit(amount);
+    }
 }
