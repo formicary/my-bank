647ca872822ce23dcf16679249c9419cfc2ae181
EntityMordecai <psysmw@nottingham.ac.uk>
2019-08-16 19:02:50 +0100

    delete unnecessary commented out code and reformatted some if statements.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -36,23 +36,6 @@ public abstract class Account {
     }
 
     public abstract double interestEarned();
-//    public double interestEarned() {
-//        double amount = sumTransactions();
-//
-//        switch(accountType){
-//            case SAVINGS:
-//                if (amount <= 1000)
-//                    return amount * 0.001;
-//                else
-//                    return 1 + (amount-1000) * 0.002;
-//            case MAXI_SAVINGS:
-//                if (amount <= 1000) return amount * 0.02;
-//                if (amount <= 2000) return 20 + (amount-1000) * 0.05;
-//                return 70 + (amount-2000) * 0.1;
-//            default:
-//                return amount * 0.001;
-//        }
-//    }
 
     public double getAccountValue() {
         return accountValue;
