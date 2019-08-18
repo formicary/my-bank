12c3e37b39ebdcfeff8d06fc5a4372263ac8d462
EntityMordecai <psysmw@nottingham.ac.uk>
2019-08-18 16:23:24 +0100

    removed redundant function.

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -14,32 +14,26 @@ public class Bank {
     }
 
     public String customerSummary() {
-        String summary = "Customer Summary";
-        for (Customer c : customers)
-            summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
-        return summary;
+        StringBuilder summary = new StringBuilder("Customer Summary");
+
+        for (Customer customer : customers)
+            summary.append("\n - ").append(customer.getName()).append(" (").append(handlePluralAccount(customer.getNumberOfAccounts())).append(")");
+
+        return summary.toString();
     }
 
     //Make sure correct plural of word is created based on the number passed in:
     //If number passed in is 1 just return the word otherwise add an 's' at the end
-    private String format(int number, String word) {
-        return number + " " + (number == 1 ? word : word + "s");
+    private String handlePluralAccount(int number) {
+        return number + " " + (number == 1 ? "account" : "accounts");
     }
 
     public double totalInterestPaid() {
         double total = 0;
-        for(Customer c: customers)
-            total += c.totalInterestEarned();
-        return total;
-    }
 
-    public String getFirstCustomer() {
-        try {
-            customers = null;
-            return customers.get(0).getName();
-        } catch (Exception e){
-            e.printStackTrace();
-            return "Error";
-        }
+        for(Customer customer : customers)
+            total += customer.totalInterestEarned();
+
+        return total;
     }
 }
