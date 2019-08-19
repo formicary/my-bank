758949a4d03b47725717978ff6ccbfec70365087
EntityMordecai <psysmw@nottingham.ac.uk>
2019-08-19 15:14:09 +0100

    add tests for account.

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -3,9 +3,11 @@ package com.abc;
 import org.junit.Test;
 
 import java.util.Calendar;
-import java.util.Date;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class AccountTest {
     public class TestMaxiSavingsAccount extends MaxiSavingsAccount {
@@ -33,6 +35,102 @@ public class AccountTest {
     }
 
     private static final double DOUBLE_DELTA = 1e-15;
+
+    @Test
+    public void testSavingsAccountConstructor() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+
+        assertEquals("Savings Account", savingsAccount.getAccountType());
+    }
+
+    @Test
+    public void testMaxiSavingsAccountConstructor() {
+        MaxiSavingsAccount maxiSavingsAccount = new MaxiSavingsAccount();
+
+        assertEquals("Maxi Savings Account", maxiSavingsAccount.getAccountType());
+    }
+
+    @Test
+    public void testOpenCheckingAccount() {
+        CheckingAccount checkingAccount = new CheckingAccount();
+
+        assertEquals("Checking Account", checkingAccount.getAccountType());
+    }
+
+    @Test
+    public void testDepositLessThanZero() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+        try {
+            savingsAccount.deposit(-100);
+            fail("Expected exception not thrown");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("amount must be greater than zero"));
+        }
+
+    }
+
+    @Test
+    public void testDepositZero() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+        try {
+            savingsAccount.deposit(0);
+            fail("Expected exception not thrown");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("amount must be greater than zero"));
+        }
+    }
+
+    @Test
+    public void testDepositGreaterThanZero() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+        savingsAccount.deposit(100);
+
+        assertEquals(100, savingsAccount.getAccountValue(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void testWithdrawLessThanZero() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+        try {
+            savingsAccount.deposit(100);
+            savingsAccount.withdraw(-40);
+            fail("Expected exception not thrown");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("amount must be greater than zero"));
+        }
+    }
+
+    @Test
+    public void testWithdrawZero() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+        try {
+            savingsAccount.deposit(10);
+            savingsAccount.withdraw(0);
+            fail("Expected exception not thrown");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("amount must be greater than zero"));
+        }
+    }
+
+    @Test
+    public void testWithdrawGreaterThanZeroLessThanAccountValue() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+            savingsAccount.deposit(100);
+            savingsAccount.withdraw(50);
+            assertEquals(50, savingsAccount.getAccountValue(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void testWithdrawGreaterThanZeroGreaterThanAccountValue() {
+        SavingsAccount savingsAccount = new SavingsAccount();
+        try {
+            savingsAccount.deposit(100);
+            savingsAccount.withdraw(110);
+            fail("Expected exception not thrown");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("amount must be less than the total value of the account"));
+        }
+    }
 
     @Test
     public void testTotalInterestEarnedSavingsAccountUnderOneThousand() {
