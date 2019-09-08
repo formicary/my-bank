f40620b500ca53ac03fec6ee4655ec71098483c1
Delzkie <renzel.david@gmail.com>
2019-09-08 22:05:43 +0100

    Basic formatting

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -4,15 +4,16 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateProvider {
-    private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	private static DateProvider instance = null;
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	public static DateProvider getInstance() {
+		if (instance == null)
+			instance = new DateProvider();
+		return instance;
+	}
+
+	public Date now() {
+		return Calendar.getInstance().getTime();
+	}
 }
--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,14 @@
 package com.abc;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
 public class TransactionTest {
-    @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
-    }
+
+	@Test
+	public void transaction() {
+		Transaction t = new Transaction(5);
+		assertTrue(t instanceof Transaction);
+	}
 }
