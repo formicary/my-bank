2cc5b2a11832b93f5088db0b61b2ea7ad684f825
Kyle <kyle.jake.kw@gmail.com>
2019-09-13 22:08:05 +0100

    refactor statements

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -9,7 +9,7 @@ import static java.lang.Math.abs;
 
 public class Customer {
     private String name;
-    private List<Account> accounts;
+    private ArrayList<Account> accounts;
 
     public Customer(String name) {
         this.name = name;
@@ -28,36 +28,15 @@ public class Customer {
         return accounts.size();
     }
 
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
     public double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts)
             total += a.interestEarned();
         return total;
-    }
-
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.calculateBalance();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
-    }
-
-    private String statementForAccount(Account a) {//TODO
-        String s = "";
-        s = s + a.getName() + "\n";
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.getTransactions()) {
-            s += "  " + (t.getAmount() < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.getAmount()) + "\n";
-            total += t.getAmount();
-        }
-        s += "Total " + toDollars(total);
-        return s;
     }
 
     private String toDollars(double d){
---
+++ src/main/java/com/abc/StatementGenerator.java
@@ -0,0 +1,47 @@
+package com.abc;
+
+import com.abc.Accounts.Account;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.abs;
+
+public class StatementGenerator {
+    private Customer customer;
+
+    public StatementGenerator(Customer customer){
+        this.customer = customer;
+    }
+
+    public String getStatement() {
+        ArrayList<Account> accounts = customer.getAccounts();
+        String statement = null;
+        statement = "Statement for " + customer.getName() + "\n";
+        double total = 0.0;
+        for (Account a : accounts) {
+            statement += "\n" + statementForAccount(a) + "\n";
+            total += a.calculateBalance();
+        }
+        statement += "\nTotal In All Accounts " + toDollars(total);
+        return statement;
+    }
+
+    private String statementForAccount(Account a) {
+        String s = "";
+        s = s + a.getName() + "\n";
+        //Now total up all the transactions
+        double total = 0.0;
+        for (Transaction t : a.getTransactions()) {
+            s += "  " + (t.getAmount() < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.getAmount()) + "\n";
+            total += t.getAmount();
+        }
+        s += "Total " + toDollars(total);
+        return s;
+    }
+
+    private String toDollars(double d){
+        return String.format("$%,.2f", abs(d));
+    }
+
+
+}
