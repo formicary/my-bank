5b55da8f6c88d6e9aec02b4a3816cec779468240
Kyle <kyle.jake.kw@gmail.com>
2019-09-15 18:15:58 +0100

    refactor transactions test to test a functional component

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -2,12 +2,12 @@ package com.abc;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class TransactionTest {
     @Test
-    public void transaction() {
+    public void getAmount() {
         Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
+        assertEquals(5.0, t.getAmount());
     }
 }
