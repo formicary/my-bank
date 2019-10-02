4d4501b672b7676d301787ddfca8788f24844317
Freddie Ager <freddieager@gmail.com>
2019-10-02 17:26:37 +0100

    Account transactions now have times which default to the current time

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,34 +1,50 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public abstract class Account {
 
 	protected List<Transaction> transactions;
+	protected Date lastWithdrawalDate;
 
     public Account() {
         this.transactions = new ArrayList<Transaction>();
+        this.lastWithdrawalDate = new GregorianCalendar(1970, 0, 0).getTime();
     }
         
     @Override
     public abstract String toString();
     
-    public void deposit(double amount) throws IllegalArgumentException {
+    public void deposit(double amount) {
+    	deposit(amount, Calendar.getInstance().getTime());
+    }
+        
+    public void deposit(double amount, Date transactionDate) throws IllegalArgumentException {
         if (amount <= 0) {
             throw new IllegalArgumentException("Amount must be greater than zero");
         } 
         else {
-            transactions.add(new Transaction(amount));
+            transactions.add(new Transaction(amount, transactionDate));
         }
     }
+    
+    public void withdraw(double amount) {
+    	withdraw(amount, Calendar.getInstance().getTime());
+    }
 	
-	public void withdraw(double amount) throws IllegalArgumentException {
+	public void withdraw(double amount, Date transactionDate) throws IllegalArgumentException {
 	    if (amount <= 0) {
 	        throw new IllegalArgumentException("Amount must be greater than zero");
 	    } 
 	    else {
-	        transactions.add(new Transaction(-amount));
+	        transactions.add(new Transaction(-amount, transactionDate));
+	        if (transactionDate.compareTo(lastWithdrawalDate) > 0) {
+	        	lastWithdrawalDate = transactionDate;
+	        }
 	    }
 	}
 	
