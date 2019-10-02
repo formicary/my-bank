7d283399a398f698edc041eaa7e8ff4ec8178a3d
Freddie Ager <freddieager@gmail.com>
2019-10-02 15:27:39 +0100

    Refactored Customer

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -3,8 +3,6 @@ package com.abc;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Math.abs;
-
 public class Customer {
     private String name;
     private List<Account> accounts;
@@ -18,9 +16,8 @@ public class Customer {
         return name;
     }
 
-    public Customer openAccount(Account account) {
+    public void openAccount(Account account) {
         accounts.add(account);
-        return this;
     }
 
     public int getNumberOfAccounts() {
@@ -29,50 +26,25 @@ public class Customer {
 
     public double totalInterestEarned() {
         double total = 0;
-        for (Account a : accounts)
-            total += a.interestEarned();
+        for (Account account : accounts)
+            total += account.getInterest();
         return total;
     }
 
     public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
+        String statement = "Statement for " + name + "\n"; 
+        for (Account account : accounts) {
+            statement += "\n" + account.getStatement() + "\n";
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
+        statement += String.format("\nTotal for all accounts: $%.2f", sumAccounts());
         return statement;
     }
-
-    private String statementForAccount(Account a) {
-        String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
-
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
-        return s;
-    }
-
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
+    
+    private double sumAccounts() {
+    	double sum = 0;
+    	for (Account accounts : accounts) {
+    		sum += accounts.sumTransactions();
+    	}
+    	return sum;
     }
 }
