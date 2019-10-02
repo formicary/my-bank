47b14a42630ad671bc86a5804802a1b79e5e7742
Freddie Ager <freddieager@gmail.com>
2019-10-02 15:27:23 +0100

    Refactored Transaction

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -4,13 +4,27 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Transaction {
-    public final double amount;
-
-    private Date transactionDate;
+	
+ 	private final double amount;
+    private final Date transactionDate;
 
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = Calendar.getInstance().getTime();
     }
+    
+    public double getAmount() {
+ 		return amount;
+ 	}
+
+ 	public Date getTransactionDate() {
+ 		return transactionDate;
+ 	}
+ 	
+ 	@Override
+ 	public String toString() {
+ 		String type = (amount > 0) ? "Deposit" : "Withdrawal";
+		return String.format("  %s $%.2f", type, Math.abs(amount));
+ 	}
 
 }
