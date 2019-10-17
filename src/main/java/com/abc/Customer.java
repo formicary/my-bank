d33869a93bd2ed2bf9c57675ede91e4de08e01a0
Jake <janiemiec20@gmail.com>
2019-10-17 11:45:43 +0100

    Made Customer class elements package-private

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -5,36 +5,36 @@ import java.util.List;
 
 import static java.lang.Math.abs;
 
-public class Customer {
+class Customer {
     private String name;
     private List<Account> accounts;
 
-    public Customer(String name) {
+    Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
-    public String getName() {
+    String getName() {
         return name;
     }
 
-    public Customer openAccount(Account account) {
+    Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
 
-    public int getNumberOfAccounts() {
+    int getNumberOfAccounts() {
         return accounts.size();
     }
 
-    public double totalInterestEarned() {
+    double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts)
             total += a.interestEarned();
         return total;
     }
 
-    public String getStatement() {
+    String getStatement() {
         String statement = null;
         statement = "Statement for " + name + "\n";
         double total = 0.0;
