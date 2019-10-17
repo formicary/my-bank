73da9b5c81cff877c00939b645d033addf446775
Jake <janiemiec20@gmail.com>
2019-10-17 11:46:58 +0100

    Made Transaction class elements package-private

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -3,12 +3,12 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
-public class Transaction {
-    public final double amount;
+class Transaction {
+    final double amount;
 
     private Date transactionDate;
 
-    public Transaction(double amount) {
+    Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
     }
