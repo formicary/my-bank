64f7b2c575f81ab7ecfd7899f9097e6023241c2f
jaaferh <jaaferluay@gmail.com>
2019-10-19 12:32:57 +0100

    Adjusted sumTransactions and changed name of checkIfTransactionsExist + Added JavaDocs

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -4,19 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
-
+    
+    // Constant variables 
     public static final int CHECKING = 0;
     public static final int SAVINGS = 1;
     public static final int MAXI_SAVINGS = 2;
 
+    // Variables
     private final int accountType;
     public List<Transaction> transactions;
 
+    
+    /**
+     * Account class constructor 
+     * @param accountType CHECKING, SAVINGS or MAXI_SAVINGS
+     */
     public Account(int accountType) {
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
     }
 
+    /**
+     * Adds a positive double into the array list of transactions in the form of
+     * an account deposit. 
+     * @param amount Double value to deposit
+     */
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
@@ -25,6 +37,11 @@ public class Account {
         }
     }
 
+    /**
+     * Adds a negative double into the array list of transactions in the form of
+     * an account withdrawal. 
+     * @param amount Double value to withdraw
+     */
     public void withdraw(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
@@ -33,11 +50,16 @@ public class Account {
         }
     }
 
+    /**
+     * Calculates the amount of interest earned based on which account is in use
+     * and the total amount in the account through sumTransactions()
+     * @return double Amount of interest earned
+     */
     public double interestEarned() {
         double amount = sumTransactions();
 
         // Interest earned is 0 if amount is 0 or a negative number
-        if (amount < 1) {
+        if (amount <= 0 ) {
             return 0;
         }
 
@@ -61,7 +83,7 @@ public class Account {
             }
             return 70 + (amount - 2000) * 0.1;
 
-        default:
+        default: // CHECKING
             if (amount > 1) {
                 return amount * 0.001;
             } else {
@@ -70,18 +92,36 @@ public class Account {
         }
     }
 
+    /**
+     * Function that checks if transactions exist in the array list and calculates
+     * the total amount if true
+     * @return double Total amount in account
+     */
     public double sumTransactions() {
-        return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t : transactions) {
-            amount += t.amount;
+        if (transactionsExist()) {
+            double amount = 0.0;
+            for (Transaction t : transactions) {
+                amount += t.amount;
+            }
+            return amount;
+        } else {
+            return 0;
         }
-        return amount;
     }
 
+    
+    /**
+     * Checks if the transactions array list is empty or not
+     * @return Boolean True (not empty) or False (empty)
+     */
+    private Boolean transactionsExist() {
+        return !transactions.isEmpty();
+    }
+
+    /**
+     * Returns the account type in the form of an integer
+     * @return int 0 (CHECKING), 1 (SAVINGS) or 2 (MAXI_SAVINGS)
+     */
     public int getAccountType() {
         return accountType;
     }
