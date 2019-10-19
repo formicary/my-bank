245724c9901f26dd877b515b12eefb744899cfba
jaaferh <jaaferluay@gmail.com>
2019-10-19 16:01:23 +0100

    Added JavaDoc to Customer.java

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -9,24 +9,47 @@ public class Customer {
     private String name;
     private List<Account> accounts;
 
+    /**
+     * Customer class constructor. Takes a name as a parameter and initialises
+     * the accounts array list
+     * @param name String containing the name of the customer
+     */
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
+    /**
+     * Returns name of the customer
+     * @return name String name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Adds the the given Account parameter to the accounts array list and returns
+     * the adjusted Customer object
+     * @param account Account object
+     * @return Customer The Customer object on which the function was called
+     */
     public Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
 
+    /**
+     * Returns the size of the accounts array list
+     * @return int Number of accounts
+     */
     public int getNumberOfAccounts() {
         return accounts.size();
     }
 
+    /**
+     * Returns the total interest earned over all accounts the customer owns
+     * @return double Amount of interest
+     */
     public double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts) {
@@ -35,6 +58,11 @@ public class Customer {
         return total;
     }
 
+    /**
+     * Adds the customer name, account information (from statementForAccount)
+     * and account totals (sumTransactions) for each account to a template
+     * @return String Complete statement in dollar format
+     */
     public String getStatement() {
         String statement = null;
         statement = "Statement for " + name + "\n";
@@ -47,6 +75,12 @@ public class Customer {
         return statement;
     }
 
+    /**
+     * Identifies the account type and totals the transactions for that account.
+     * Returns this information as a String
+     * @param a Account object associated with the customer
+     * @return String Statement containing account type and total in dollar format
+     */
     private String statementForAccount(Account a) {
         String s = "";
 
@@ -73,6 +107,11 @@ public class Customer {
         return s;
     }
 
+    /**
+     * Converts a double to a String in the form of $0.00
+     * @param d Double value of amount to be converted
+     * @return String Amount converted to String value
+     */
     private String toDollars(double d) {
         return String.format("$%,.2f", abs(d));
     }
