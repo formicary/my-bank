7b992cebc0578961a6b77dfd5360bc12838ee920
krishanjaimistry <57006249+krishanjaimistry@users.noreply.github.com>
2019-10-31 16:24:12 +0000

    Update BankTest

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -2,53 +2,97 @@ package com.abc;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.Assert.*;
 
 public class BankTest {
-    private static final double DOUBLE_DELTA = 1e-15;
+
+    @Test
+    public void testAddCustomer() {
+        Bank testBank = new Bank("My testBank");
+
+        Customer Arnold = new Customer("Arnold");
+        Customer Louie = new Customer("Louie");
+
+        testBank.addCustomer(Arnold);
+
+        testBank.addCustomer(Louie);
+        assertEquals(2,testBank.numberOfCustomers());
+    }
 
     @Test
     public void customerSummary() {
-        Bank bank = new Bank();
-        Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
-        bank.addCustomer(john);
+        Bank testBank = new Bank("My testBank");
 
-        assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
+        Customer Arnold = new Customer("Arnold");
+        Customer Louie = new Customer("Louie");
+
+        testBank.addCustomer(Arnold);
+
+        testBank.addCustomer(Louie);
+
+        Arnold.openNewAccount(AccountType.CHECKING, "Arnold's Checking account");
+        Arnold.openNewAccount(AccountType.SAVINGS, "Arnold's Savings account");
+        Arnold.openNewAccount(AccountType.MAXI_SAVINGS, "Arnold's Maxi_saving account");
+        Arnold.depositIntoAccount(10,"Arnold's Checking account");
+        Arnold.depositIntoAccount(20,"Arnold's Checking account");
+        Arnold.withDrawFromAccount(20,"Arnold's Checking account");
+        Arnold.depositIntoAccount(400,"Arnold's Savings account");
+        Arnold.withDrawFromAccount(400,"Arnold's Savings account", Instant.now().plus(50, ChronoUnit.DAYS));
+
+
+        Louie.openNewAccount(AccountType.CHECKING, "Louie's Checking account");
+        Louie.openNewAccount(AccountType.SAVINGS, "Louie's Savings account");
+        Louie.openNewAccount(AccountType.MAXI_SAVINGS, "Louie's Maxi_saving account");
+        Louie.depositIntoAccount(10, "Louie's Checking account");
+        Louie.depositIntoAccount(20, "Louie's Checking account");
+        Louie.withDrawFromAccount(20, "Louie's Checking account");
+        Louie.depositIntoAccount(400, "Louie's Savings account");
+        Louie.withDrawFromAccount(400, "Louie's Savings account", Instant.now().plus(50, ChronoUnit.DAYS));
+
+        String expectedCustomerSummary = "Customer Summary for My testBank\n2 Customers\n - Arnold (3 accounts)"+
+                                        "\n - Louie (3 accounts)";
+        assertEquals(expectedCustomerSummary,testBank.customerSummary());
+
     }
 
     @Test
-    public void checkingAccount() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.CHECKING);
-        Customer bill = new Customer("Bill").openAccount(checkingAccount);
-        bank.addCustomer(bill);
+    public void testTotalInterestPaid() {
 
-        checkingAccount.deposit(100.0);
+        Bank testBank = new Bank("My testBank");
 
-        assertEquals(0.1, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
+        Customer Arnold = new Customer("Arnold");
+        Customer Louie = new Customer("Louie");
 
-    @Test
-    public void savings_account() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
+        testBank.addCustomer(Arnold);
 
-        checkingAccount.deposit(1500.0);
+        testBank.addCustomer(Louie);
 
-        assertEquals(2.0, bank.totalInterestPaid(), DOUBLE_DELTA);
-    }
+        Arnold.openNewAccount(AccountType.CHECKING, "Arnold's Checking account");
+        Arnold.openNewAccount(AccountType.SAVINGS, "Arnold's Savings account");
+        Arnold.openNewAccount(AccountType.MAXI_SAVINGS, "Arnold's Maxi_saving account");
+        Arnold.depositIntoAccount(10,"Arnold's Checking account");
+        Arnold.depositIntoAccount(20,"Arnold's Checking account");
+        Arnold.withDrawFromAccount(20,"Arnold's Checking account");
+        Arnold.depositIntoAccount(400,"Arnold's Savings account");
+        Arnold.withDrawFromAccount(400,"Arnold's Savings account", Instant.now().plus(50, ChronoUnit.DAYS));
 
-    @Test
-    public void maxi_savings_account() {
-        Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
-        bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
-        checkingAccount.deposit(3000.0);
+        Louie.openNewAccount(AccountType.CHECKING, "Louie's Checking account");
+        Louie.openNewAccount(AccountType.SAVINGS, "Louie's Savings account");
+        Louie.openNewAccount(AccountType.MAXI_SAVINGS, "Louie's Maxi_saving account");
+        Louie.depositIntoAccount(10, "Louie's Checking account");
+        Louie.depositIntoAccount(20, "Louie's Checking account");
+        Louie.withDrawFromAccount(20, "Louie's Checking account");
+        Louie.depositIntoAccount(400, "Louie's Savings account");
+        Louie.withDrawFromAccount(400, "Louie's Savings account", Instant.now().plus(50, ChronoUnit.DAYS));
 
-        assertEquals(170.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+        assertEquals(0.10959639739508567,testBank.totalInterestPaid(),1e-9);
+
+
+
     }
 
 }
