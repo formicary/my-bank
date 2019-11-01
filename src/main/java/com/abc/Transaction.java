e95a21c79341c0e4f323cd124d1907c49aa7752a
eliasavva <elia321@hotmail.co.uk>
2019-11-01 11:03:07 +0000

    Update Transaction.java
    general tidy up

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -4,22 +4,25 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Transaction {
-    public final double amount;
-    public static final int DEPOSIT = 0;
+	public final double amount;
+	// added transaction type for the second added feature
+	public static final int DEPOSIT = 0;
 	public static final int WITHDRAWAL = 1;
 	private final int transactionType;
-    private Date transactionDate;
+	private Date transactionDate;
 
-    public Transaction(double amount, int type) {
-        this.amount = amount;
-        this.transactionDate = Calendar.getInstance().getTime();
-        this.transactionType = type;
-    }
-    public Date getDate() {
-    	return transactionDate;
-    }
-    public int getTransactionType() {
-    	return this.transactionType;
-    }
+	public Transaction(double amount, int type) {
+		this.amount = amount;
+		this.transactionDate = Calendar.getInstance().getTime();
+		this.transactionType = type;
+	}
+
+	public Date getDate() {
+		return transactionDate;
+	}
+
+	public int getTransactionType() {
+		return this.transactionType;
+	}
 
 }
