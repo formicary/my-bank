97015a991e352bd8368ab989541e8cc2555443c3
Cristi Beldie <cristianbeldie96@gmail.com>
2019-11-29 15:11:03 +0200

    Refactored MaxiSavingsAccount.java; removed unnecessary static vars

--- src/main/java/com/abc/MaxiSavingsAccount.java
+++ src/main/java/com/abc/MaxiSavingsAccount.java
@@ -3,17 +3,9 @@ package com.abc;
 import java.math.BigDecimal;
 
 public class MaxiSavingsAccount extends Account {
-    // Maxi-savings account rates declared previously
-    private static final double INITIAL_RATE = 0.02;
-    private static final double SECOND_RATE = 0.05;
-    private static final double THIRD_RATE = 0.1;
-    // Maxi-Savings account thresholds to trigger different rates
-    private static final int FIRST_THRESHOLD = 1000;
-    private static final int SECOND_THRESHOLD = 2000;
-
-    // NEW RATES AS PER NEW FEATURE - TESTING
-    private static final double NEW_RATE1 = 0.001;
-    private static final double NEW_RATE2 = 0.05;
+    // RATES ESTABLISHED PREVIOUSLY
+    private static final double WITHDRAW_SMALLER_10 = 0.001;
+    private static final double WITHDRAW_LARGER_10 = 0.05;
 
 
     public MaxiSavingsAccount() {
@@ -23,8 +15,8 @@ public class MaxiSavingsAccount extends Account {
     public BigDecimal interestEarned() {
         BigDecimal balance = getBalance();
         BigDecimal interest = daysSinceLastWithdraw() >= 10
-                ? balance.multiply(BigDecimal.valueOf(NEW_RATE2))
-                : balance.multiply(BigDecimal.valueOf(NEW_RATE1));
+                ? balance.multiply(BigDecimal.valueOf(WITHDRAW_LARGER_10))
+                : balance.multiply(BigDecimal.valueOf(WITHDRAW_SMALLER_10));
 
         return interest;
     }
