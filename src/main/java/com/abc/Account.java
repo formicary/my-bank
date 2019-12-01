c9314b3f9294f10ca0c2762bda706f2def368de6
yulijag <yulijag@outlook.com>
2019-12-01 15:42:28 +0000

    Changes Account types to enum var

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -4,15 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Account {
+	
+	enum Type {CHECKING, SAVINGS, MAXI_SAVINGS}
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
-
-    private final int accountType;
+    private final Type accountType;
     public List<Transaction> transactions;
 
-    public Account(int accountType) {
+    public Account(Type accountType) {
         this.accountType = accountType;
         this.transactions = new ArrayList<Transaction>();
     }
@@ -25,13 +23,13 @@ public class Account {
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+	public void withdraw(double amount) {
+	    if (amount <= 0) {
+	        throw new IllegalArgumentException("amount must be greater than zero");
+	    } else {
+	        transactions.add(new Transaction(-amount));
+	    }
+	}
 
     public double interestEarned() {
         double amount = sumTransactions();
@@ -66,7 +64,7 @@ public void withdraw(double amount) {
         return amount;
     }
 
-    public int getAccountType() {
+    public Type getAccountType() {
         return accountType;
     }
 
--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -11,7 +11,7 @@ public class BankTest {
     public void customerSummary() {
         Bank bank = new Bank();
         Customer john = new Customer("John");
-        john.openAccount(new Account(Account.CHECKING));
+        john.openAccount(new Account(Account.Type.CHECKING));
         bank.addCustomer(john);
 
         assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
@@ -20,7 +20,7 @@ public class BankTest {
     @Test
     public void checkingAccount() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.CHECKING);
+        Account checkingAccount = new Account(Account.Type.CHECKING);
         Customer bill = new Customer("Bill").openAccount(checkingAccount);
         bank.addCustomer(bill);
 
@@ -32,7 +32,7 @@ public class BankTest {
     @Test
     public void savings_account() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.SAVINGS);
+        Account checkingAccount = new Account(Account.Type.SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
         checkingAccount.deposit(1500.0);
@@ -43,7 +43,7 @@ public class BankTest {
     @Test
     public void maxi_savings_account() {
         Bank bank = new Bank();
-        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        Account checkingAccount = new Account(Account.Type.MAXI_SAVINGS);
         bank.addCustomer(new Customer("Bill").openAccount(checkingAccount));
 
         checkingAccount.deposit(3000.0);
