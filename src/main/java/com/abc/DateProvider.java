b75c240c5af6d932b0d2d5ff366c6753b25e0abc
yulijag <yulijag@outlook.com>
2019-12-05 02:30:22 +0000

    Adds date manipulation feature (set date to next day)

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -1,18 +1,25 @@
 package com.abc;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.LocalDate;
 
 public class DateProvider {
     private static DateProvider instance = null;
+    private Clock clock = Clock.systemDefaultZone();
 
     public static DateProvider getInstance() {
         if (instance == null)
             instance = new DateProvider();
         return instance;
     }
-
-    public Date now() {
-        return Calendar.getInstance().getTime();
+    
+    // set the date to next day - used for testing
+    public void nextDay() {
+    	clock = Clock.offset(clock, Duration.ofDays(1));
+    }
+    
+    public LocalDate now() {
+    	return LocalDate.now(clock);
     }
 }
