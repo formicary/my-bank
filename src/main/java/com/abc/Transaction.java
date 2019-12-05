ad065e96bf9782c6ff24c922f18e8971f8a18c9e
yulijag <yulijag@outlook.com>
2019-12-05 02:13:19 +0000

    Updates transaction class and testing

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -4,26 +4,45 @@ import java.time.LocalDate;
 
 public class Transaction {
 	
+	// Transaction types for simpler printing
+	public enum Type {
+		WITHDRAW ("withdrawal"), DEPOSIT ("deposit"), TRANSFER ("transfer"), INTERESTS ("interests");
+		private final String type;
+	
+	    private Type(final String type) { this.type = type; }
+	    public String toString() { return type; }
+		
+	};
+	
     private final double amount;
     private LocalDate transactionDate;
+    private Type type;
+    private String toFromAccountID;
 
-    public Transaction(double amount) {
+    // constructor for withdrawal, deposit and interest transactions
+    public Transaction(double amount, Type type) {
         this.amount = amount;
-        this.transactionDate = LocalDate.now();
+        this.type = type;
+        this.transactionDate = DateProvider.getInstance().now();
     }
     
-    public double getAmount() {
-    	return amount;
+    // constructor for transfer transactions to include sender/receiver ID
+    public Transaction(double amount, String account) {
+        this.amount = amount;
+        this.type = Type.TRANSFER;
+        this.transactionDate = DateProvider.getInstance().now();
+        toFromAccountID = account;
     }
     
-    public LocalDate getDate() {
-    	return transactionDate;
-    }
+    public double getAmount() { return amount; }
+    public LocalDate getDate() { return transactionDate; }
     
+    // String representation for statements
     @ Override
     public String toString() {
     	String a = String.format("$%,.2f", amount);
-    	return (amount < 0 ? "withdrawal" : "deposit") + " " + a;
+    	return transactionDate.toString() + ": " + type + " " + a 
+    			+ (type==Type.TRANSFER? (amount<0? " (To " + toFromAccountID + ")" : " (From " + toFromAccountID + ")") : "");
     }
-    
+
 }
--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,36 @@
 package com.abc;
 
 import org.junit.Test;
-
+import com.abc.Transaction.Type;
 import static org.junit.Assert.assertTrue;
+import java.time.LocalDate;
 
 public class TransactionTest {
     @Test
-    public void transaction() {
-        Transaction t = new Transaction(5);
-        assertTrue(t instanceof Transaction);
+    public void transactionTime() {
+        Transaction t = new Transaction(5, Type.DEPOSIT);
+        assertTrue(t.getDate().equals(LocalDate.now()));
+
+        DateProvider.getInstance().nextDay();
+        Transaction nextt = new Transaction(6, Type.DEPOSIT);
+        assertTrue(nextt.getDate().equals(LocalDate.now().plusDays(1)));
     }
+    
+    @Test
+    public void transactionString() {
+        Transaction wTransaction = new Transaction(-5, Type.WITHDRAW);
+        Transaction dTransaction = new Transaction(6, Type.DEPOSIT);
+        Transaction iTransaction = new Transaction(1, Type.INTERESTS);
+        Transaction t1Transaction = new Transaction(-8, "account 2");
+        Transaction t2Transaction = new Transaction(8, "account 1");
+        
+        String currentDate = DateProvider.getInstance().now().toString();
+        
+        assertTrue(wTransaction.toString().equals(currentDate + ": withdrawal $-5.00"));
+        assertTrue(dTransaction.toString().equals(currentDate + ": deposit $6.00"));
+        assertTrue(iTransaction.toString().equals(currentDate + ": interests $1.00"));
+        assertTrue(t1Transaction.toString().equals(currentDate + ": transfer $-8.00 (To account 2)"));
+        assertTrue(t2Transaction.toString().equals(currentDate + ": transfer $8.00 (From account 1)"));
+    }
+    
 }
