400daae50633c785756c7a2b54509d8360126b82
yulijag <yulijag@outlook.com>
2019-12-05 02:34:12 +0000

    Updates Customer class to include interest gains/updates + testing

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,78 +1,81 @@
 package com.abc;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.lang.Math.abs;
+import java.util.LinkedHashMap ;
 
 public class Customer {
     private String name;
-    private List<Account> accounts;
+    private LinkedHashMap<String, Account> accounts;	// accounts have id mappings for easier access and to keep them private to the customer
 
     public Customer(String name) {
         this.name = name;
-        this.accounts = new ArrayList<Account>();
+        this.accounts = new LinkedHashMap <String, Account>();
     }
 
-    public String getName() {
-        return name;
+    public String getName() { return name; }
+    public int getNumberOfAccounts() { return accounts.size(); }
+    public double getAccountBalance(String id) { return accounts.get(id).getBalance(); }
+    
+
+    public String openAccount(Account.Type type) {
+    	String id = ""+accounts.size();		// id is just an incremental value for simplicity, but can be changed to UUID
+        accounts.put(id, new Account(type));
+    	
+    	return id;
+    }
+    
+    public void deposit(String accountID, double amount) {
+    	if (!accounts.containsKey(accountID)) throw new IllegalArgumentException("tryint to deposit to non-existent account");
+    	accounts.get(accountID).deposit(amount);
+    }
+    
+    public void withdraw(String accountID, double amount) {
+    	if (!accounts.containsKey(accountID)) throw new IllegalArgumentException("tryint to withdraw from non-existent account");
+    	accounts.get(accountID).withdraw(amount);
+    }
+    
+    public void transfer(String fromID, String toID, double amount) {
+    	if (!accounts.containsKey(fromID) || !accounts.containsKey(toID)) { 
+    		throw new IllegalArgumentException("tryint to transfer to/from non-existent account");
+    	}
+    	accounts.get(fromID).transfer(-amount, toID);
+    	accounts.get(toID).transfer(amount, fromID);
     }
 
-    public Customer openAccount(Account account) {
-        accounts.add(account);
-        return this;
-    }
-
-    public int getNumberOfAccounts() {
-        return accounts.size();
-    }
-
-    public double totalInterestEarned() {
+    // updates all accounts and returns the daily interest earned  
+    public double earnDailyInterest() {
         double total = 0;
-        for (Account a : accounts)
-            total += a.interestEarned();
-        return total;
+        for (Account a : accounts.values()) {
+            total += a.earnInterest();
+        }
+        return twoDecimal(total);
     }
-
+    
+    // returns the total interest earned from the beginning
+    public double getTotalInterestEarned() {
+    	 double total = 0;
+         for (Account a : accounts.values()) {
+             total += a.getTotalInterestEarned();
+         }
+         return twoDecimal(total);
+    }
+    
     public String getStatement() {
         String statement = null;
         statement = "Statement for " + name + "\n";
+        
         double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
+        for (String a : accounts.keySet()) {
+        	statement += "\n" + "Account " + a + "\n";
+            statement += accounts.get(a).getStatement() + "\n";
+            total += accounts.get(a).getBalance();
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
+        statement += "\nTotal In All Accounts " + String.format("$%,.2f",total);
         return statement;
     }
-
-    private String statementForAccount(Account a) {
-        String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case CHECKING:
-                s += "Checking Account\n";
-                break;
-            case SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
-
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
-        return s;
+    
+    // helper method to get 2 decimal precision
+    private double twoDecimal(double n) {
+    	return Double.parseDouble(String.format("%.2f", n)); 
     }
-
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
-    }
+  
 }
--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -1,57 +1,184 @@
 package com.abc;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.time.LocalDate;
+import java.util.Random;
 
 public class CustomerTest {
+	
+    private static final double DOUBLE_DELTA = 1e-15;
 
     @Test //Test customer statement generation
-    public void testApp(){
+    public void testStatement(){
 
-        Account checkingAccount = new Account(Account.Type.CHECKING);
-        Account savingsAccount = new Account(Account.Type.SAVINGS);
+        Customer henry = new Customer("Henry");
+        String checkingAccountID = henry.openAccount(Account.Type.CHECKING);
+        String savingsAccountID = henry.openAccount(Account.Type.SAVINGS);
 
-        Customer henry = new Customer("Henry").openAccount(checkingAccount).openAccount(savingsAccount);
-
-        checkingAccount.deposit(100.0);
-        savingsAccount.deposit(4000.0);
-        savingsAccount.withdraw(200.0);
+        henry.deposit(checkingAccountID, 100.0);
+        henry.transfer(checkingAccountID, savingsAccountID, 50.0);
+        henry.withdraw(savingsAccountID, 20.0);
+        nextDayInterestIssue(henry);			// imitates a day passing to update interest rates
+        
+        LocalDate currentDate = DateProvider.getInstance().now().minusDays(1);
 
         assertEquals("Statement for Henry\n" +
                 "\n" +
+        		"Account " + checkingAccountID + "\n" + 
                 "Checking Account\n" +
-                "  deposit $100.00\n" +
-                "Total $100.00\n" +
+                "  "+ currentDate +": deposit $100.00\n" +
+                "  "+ currentDate +": transfer $-50.00 (To " + savingsAccountID + ")\n" +
+                "  "+ currentDate.plusDays(1) +": interests $0.05\n" +
+                "Total $50.05\n" +
                 "\n" +
+                "Account " + savingsAccountID + "\n" + 
                 "Savings Account\n" +
-                "  deposit $4,000.00\n" +
-                "  withdrawal $200.00\n" +
-                "Total $3,800.00\n" +
+                "  "+ currentDate +": transfer $50.00 (From " + checkingAccountID + ")\n" +
+                "  "+ currentDate +": withdrawal $-20.00\n" +
+                "  "+ currentDate.plusDays(1) +": interests $0.03\n" +
+                "Total $30.03\n" +
                 "\n" +
-                "Total In All Accounts $3,900.00", henry.getStatement());
+                "Total In All Accounts $80.08", henry.getStatement());
     }
+
+    // test number of accounts
+    @Test
+    public void testAccountNumber(){
+        Customer oscar = new Customer("Oscar");
+        int n = new Random().nextInt(10);
+        for (int i=0; i<n; i++) {
+        	oscar.openAccount(Account.Type.SAVINGS);
+        }       
+        assertEquals(n, oscar.getNumberOfAccounts());
+    }
+    
+    // test transfer feature
+    @Test
+    public void testTransfer(){
+        Customer henry = new Customer("Henry");
+        String checkingAccountID = henry.openAccount(Account.Type.CHECKING);
+        String savingsAccountID = henry.openAccount(Account.Type.SAVINGS);
+        henry.deposit(checkingAccountID, 1000.0);
+        henry.deposit(savingsAccountID, 1000.0);
+        
+        int n = new Random().nextInt(600)+1;
+        henry.transfer(checkingAccountID, savingsAccountID, n);
+        
+        assertEquals(1000-n, henry.getAccountBalance(checkingAccountID), DOUBLE_DELTA);
+        assertEquals(1000+n, henry.getAccountBalance(savingsAccountID), DOUBLE_DELTA);
+    }
+    
+    // test that exceptions are thrown correctly for invalid input
+    @Test
+    public void testExceptions(){
+    	Customer henry = new Customer("Henry");
+        String checkingAccountID = henry.openAccount(Account.Type.CHECKING);
+        String checkingAccountID2 = henry.openAccount(Account.Type.CHECKING);
+        
+        try {
+        	henry.deposit(checkingAccountID, -3);
+        	fail("Exception not thrown");
+        } catch (IllegalArgumentException expectedException) { }
+        try {
+        	henry.deposit("", 3);
+        	fail("Exception not thrown");
+        } catch (IllegalArgumentException expectedException) { }
+        try {
+        	henry.withdraw(checkingAccountID, -3);
+        	fail("Exception not thrown");
+        } catch (IllegalArgumentException expectedException) { }
+        try {
+        	henry.withdraw("", 3);
+        	fail("Exception not thrown");
+        } catch (IllegalArgumentException expectedException) { }
+        try {
+        	henry.transfer("", "", 3);
+        	fail("Exception not thrown");
+        } catch (IllegalArgumentException expectedException) { }
+        try {
+        	henry.transfer(checkingAccountID, checkingAccountID2, 3);
+        	fail("Exception not thrown");
+        } catch (IllegalArgumentException expectedException) { }
+        
+    }     
 
     @Test
-    public void testOneAccount(){
-        Customer oscar = new Customer("Oscar").openAccount(new Account(Account.Type.SAVINGS));
-        assertEquals(1, oscar.getNumberOfAccounts());
+    public void testCheckingInterest() {
+        Customer oscar = new Customer("Oscar");
+        String id = oscar.openAccount(Account.Type.CHECKING);
+        
+        int n = new Random().nextInt(50000)+50000;
+        oscar.deposit(id, n);
+        
+        double interest = twoDecimal(n*0.001);
+        		
+        assertTrue(interest==nextDayInterestIssue(oscar));
     }
-
+    
     @Test
-    public void testTwoAccount(){
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.Type.SAVINGS));
-        oscar.openAccount(new Account(Account.Type.CHECKING));
-        assertEquals(2, oscar.getNumberOfAccounts());
+    public void testSavingsInterest() {
+        Customer oscar = new Customer("Oscar");
+        String id = oscar.openAccount(Account.Type.SAVINGS);
+        oscar.deposit(id, 1000);
+        
+        assertTrue(1.0==nextDayInterestIssue(oscar));
+        
+        int n = new Random().nextInt(10000)+1000;
+        oscar.deposit(id, n);
+        
+        double interest = twoDecimal((1+n)*0.002);
+        assertEquals((1+interest), nextDayInterestIssue(oscar), DOUBLE_DELTA);      
     }
-
-    @Ignore
-    public void testThreeAcounts() {
-        Customer oscar = new Customer("Oscar")
-                .openAccount(new Account(Account.Type.SAVINGS));
-        oscar.openAccount(new Account(Account.Type.CHECKING));
-        assertEquals(3, oscar.getNumberOfAccounts());
+    
+    @Test
+    public void testMaxiSavingsInterest() {
+        Customer oscar = new Customer("Oscar");
+        String id = oscar.openAccount(Account.Type.MAXI_SAVINGS);
+        
+        int n = new Random().nextInt(50000)+50000;
+        oscar.deposit(id, n);
+        
+        double interest = twoDecimal(n*0.05);
+        assertTrue(interest==nextDayInterestIssue(oscar));
+        
+        int n1 = new Random().nextInt(50000);
+        oscar.withdraw(id, n1);
+        
+        double interest1 = twoDecimal((n+interest-n1)*0.001);
+        
+        assertTrue(interest1==nextDayInterestIssue(oscar));  
+    }
+    
+    // test that total account interest gain is calculated correctly
+    @Test
+    public void testTotalInterest() {
+    	Customer henry = new Customer("Henry");
+        String checkingAccountID = henry.openAccount(Account.Type.CHECKING);
+        String savingsAccountID = henry.openAccount(Account.Type.SAVINGS);
+        String maxisavingsAccountID = henry.openAccount(Account.Type.MAXI_SAVINGS);
+        
+        henry.deposit(checkingAccountID, 1000.0);
+        henry.deposit(savingsAccountID, 1000.0);
+        henry.deposit(maxisavingsAccountID, 1000.0);
+        
+        nextDayInterestIssue(henry);
+        
+        assertEquals(52.0, henry.getTotalInterestEarned(), DOUBLE_DELTA);
+    }
+    
+    // helper methos to imitate date change and interest update
+	private double nextDayInterestIssue(Customer c) {
+		DateProvider.getInstance().nextDay();
+		return c.earnDailyInterest();
+	}
+	
+	// helper method for 2 decimal precision
+    private double twoDecimal(double n) {
+    	return Double.parseDouble(String.format("%.2f", n)); 
     }
 }
