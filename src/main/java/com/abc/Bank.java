dbf66f26aebbef4081b6e2f134d5898f70f97507
Adil Asghar <adilraja1@hotmail.co.uk>
2019-12-07 18:12:57 +0000

    Reformat the Bank class
    - General reformatting of the Bank class

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -16,21 +16,23 @@ public class Bank {
 
     public String customerSummary() {
         String summary = "Customer Summary";
-        for (Customer c : customers)
+        for (Customer c : customers) {
             summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
+        }
         return summary;
     }
 
-    //Make sure correct plural of word is created based on the number passed in:
-    //If number passed in is 1 just return the word otherwise add an 's' at the end
+    // Make sure correct plural of word is created based on the number passed in:
+    // If number passed in is 1 just return the word otherwise add an 's' at the end
     private String format(int number, String word) {
         return number + " " + (number == 1 ? word : word + "s");
     }
 
     public double totalInterestPaid() {
         double total = 0;
-        for(Customer c: customers)
+        for (Customer c : customers) {
             total += c.totalInterestEarned();
+        }
         return total;
     }
 
@@ -38,7 +40,7 @@ public class Bank {
         try {
             customers = null;
             return customers.get(0).getName();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "Error";
         }
