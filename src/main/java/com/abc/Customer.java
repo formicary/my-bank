e347dfbfce6e3edc832bb4defac4d8147cbdc352
Adil Asghar <adilraja1@hotmail.co.uk>
2019-12-08 15:37:28 +0000

    Make some function package-private in the Customer class

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -9,7 +9,7 @@ import static java.lang.Math.abs;
  * Class to handle the customer of a bank
  */
 
-public class Customer {
+class Customer {
 
     /**
      * Name of the customer
@@ -25,7 +25,7 @@ public class Customer {
      * Constructor of the customer class
      * @param name name of the customer
      */
-    public Customer(String name) {
+    Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
@@ -34,7 +34,7 @@ public class Customer {
      * Getter for the name of the customer
      * @return name of customer
      */
-    public String getName() {
+    String getName() {
         return name;
     }
 
@@ -43,7 +43,7 @@ public class Customer {
      * @param account account to open
      * @return the customer
      */
-    public Customer openAccount(Account account) {
+    Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
@@ -52,7 +52,7 @@ public class Customer {
      * Function to get the number of accounts of a customer
      * @return number of accounts
      */
-    public int getNumberOfAccounts() {
+    int getNumberOfAccounts() {
         return accounts.size();
     }
 
@@ -60,7 +60,7 @@ public class Customer {
      * Function to calculate the total interest earned from all the accounts
      * @return total interest earned from all the accounts
      */
-    public double totalInterestEarned() {
+    double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts) {
             total += a.interestEarned();
@@ -72,7 +72,7 @@ public class Customer {
      * Function to get the statement of the customer
      * @return statement of the customer
      */
-    public String getStatement() {
+    String getStatement() {
         StringBuilder stringBuilder = new StringBuilder("Statement for " + name + "\n");
         double total = 0.0;
         for (Account a : accounts) {
