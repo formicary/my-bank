a1f3b7e9bd4804fad52d74853006873f2f916209
Adil Asghar <adilraja1@hotmail.co.uk>
2019-12-08 15:21:27 +0000

    Add comments to the TransactionTest class

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -4,10 +4,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests for the Transaction class
+ */
 public class TransactionTest {
+
     @Test
+    /**
+     * Test creating a transaction
+     */
     public void transaction() {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
     }
+
 }
