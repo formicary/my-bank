23ee84834d13ab0224560891b76a47e622e7a7ea
Adil Asghar <adilraja1@hotmail.co.uk>
2019-12-08 15:10:55 +0000

    Add comments to the Customer class

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -5,28 +5,61 @@ import java.util.List;
 
 import static java.lang.Math.abs;
 
+/**
+ * Class to handle the customer of a bank
+ */
+
 public class Customer {
+
+    /**
+     * Name of the customer
+     */
     private String name;
+
+    /**
+     * Accounts of the customer
+     */
     private List<Account> accounts;
 
+    /**
+     * Constructor of the customer class
+     * @param name name of the customer
+     */
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<Account>();
     }
 
+    /**
+     * Getter for the name of the customer
+     * @return name of customer
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * function to open an account for the customer
+     * @param account account to open
+     * @return the customer
+     */
     public Customer openAccount(Account account) {
         accounts.add(account);
         return this;
     }
 
+    /**
+     * Function to get the number of accounts of a customer
+     * @return number of accounts
+     */
     public int getNumberOfAccounts() {
         return accounts.size();
     }
 
+    /**
+     * Function to calculate the total interest earned from all the accounts
+     * @return total interest earned from all the accounts
+     */
     public double totalInterestEarned() {
         double total = 0;
         for (Account a : accounts) {
@@ -35,6 +68,10 @@ public class Customer {
         return total;
     }
 
+    /**
+     * Function to get the statement of the customer
+     * @return statement of the customer
+     */
     public String getStatement() {
         String statement = "Statement for " + name + "\n";
         double total = 0.0;
@@ -46,11 +83,16 @@ public class Customer {
         return statement;
     }
 
-    private String statementForAccount(Account a) {
+    /**
+     * Function to get the statement for an account
+     * @param account account to get the statement of
+     * @return the statement of the account
+     */
+    private String statementForAccount(Account account) {
         String s = "";
 
         // Translate to pretty account type
-        switch (a.getAccountType()) {
+        switch (account.getAccountType()) {
             case CHECKING:
                 s += "Checking Account\n";
                 break;
@@ -62,9 +104,8 @@ public class Customer {
                 break;
         }
 
-        // Now total up all the transactions
         double total = 0.0;
-        for (Transaction t : a.getTransactions()) {
+        for (Transaction t : account.getTransactions()) {
             s += "  " + (t.getAmount() < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.getAmount()) + "\n";
             total += t.getAmount();
         }
@@ -72,6 +113,11 @@ public class Customer {
         return s;
     }
 
+    /**
+     * Function to format an amount to be shown in dollars
+     * @param d amount
+     * @return formatted amount in dollars
+     */
     private String toDollars(double d) {
         return String.format("$%,.2f", abs(d));
     }
