218c36e31854c77e1dc0c9dc9f1e626c1d80ad8c
Adil Asghar <adilraja1@hotmail.co.uk>
2019-12-08 15:14:13 +0000

    Add comments to the Transaction class

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -2,19 +2,42 @@ package com.abc;
 
 import java.util.Date;
 
+/**
+ * Class to handle transaction level functions
+ */
 public class Transaction {
+
+    /**
+     * Amount of the transaction
+     */
     private final double amount;
+
+    /**
+     * Date of the transaction
+     */
     private Date transactionDate;
 
+    /**
+     * Constructor of the Transaction class
+     * @param amount amount of the transaciton
+     */
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
     }
 
+    /**
+     * Getter of the transaction date
+     * @return transaction date
+     */
     public Date getTransactionDate() {
         return transactionDate;
     }
 
+    /**
+     * Getter of the amount
+     * @return amount of the transaction
+     */
     public double getAmount() {
         return amount;
     }
