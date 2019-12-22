47edc994027d831229c2752b510b2c6d67e8dad5
Evo-Ark <Evo-Ark@user.noreply.github.com>
2019-12-22 16:46:09 +0000

    Added type of transaction to the transaction class.

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -3,13 +3,22 @@ package com.abc;
 import java.util.Date;
 
 public class Transaction {
+
     public final double amount;
-
     private Date transactionDate;
-
-    public Transaction(double amount) {
+    public enum TransactionType {
+    	DEPOSIT,
+    	WITHDRAWAL,
+    	INTEREST
+    }
+    
+    private TransactionType transactionType; 
+    
+    
+    public Transaction(double amount, TransactionType type) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
+        this.transactionType = type;
     }
     
     public Date getTransactionDate() {
