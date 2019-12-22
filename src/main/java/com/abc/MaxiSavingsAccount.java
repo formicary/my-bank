21d98c6f31553c5b3a3cf870ee8a4b44880791d2
Evo-Ark <Evo-Ark@user.noreply.github.com>
2019-12-22 15:49:26 +0000

    Implemented original yearly interest rates.

--- src/main/java/com/abc/MaxiSavingsAccount.java
+++ src/main/java/com/abc/MaxiSavingsAccount.java
@@ -2,26 +2,19 @@ package com.abc;
 
 public class MaxiSavingsAccount extends Account {
 
-    public MaxiSavingsAccount() {
-        super(Account.MAXI_SAVINGS);
-    }
+	public MaxiSavingsAccount() {
+		super(Account.MAXI_SAVINGS);
+	}
 
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
-        }
-    }
+	public double interestEarned() {
+		double amount = sumTransactions();
+		if (amount <= 1000) {
+			return amount * 0.02;
+		}
+		if (amount <= 2000) {
+			return 20 + (amount - 1000) * 0.05;
+		}
+		return 70 + (amount - 2000) * 0.1;
+
+	}
 }
