25717ead3a7e6392c8c37dc5d03eb6f5afe291b0
batuhan-ymz <59225358+batuhan-ymz@users.noreply.github.com>
2019-12-27 10:00:48 +0000

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -1,73 +1,91 @@
-package com.abc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+/*
+ * This is my Attempt at the task, I basically attempted the project from scratch and made some changed i.e finding accounts by uid
+ * these changes were made to increase flexibility/code for the future, which is also why I made some design changes to increase cohesion.
+ * I did not implement the last two additional features as I wasnt exactly sure the way it was asked to implement (didnt fully understand the description).
+ */
+
+/**
+ *
+ * @author batuhan yilmaz
+ */
 
 public class Account {
-
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
-
-    private final int accountType;
-    public List<Transaction> transactions;
-
-    public Account(int accountType) {
+    private final AccountType accountType;
+    private List<Transaction> transactions;
+    private double balance = 0;
+    private final String uniqueID ; 
+    
+    public Account(AccountType accountType) {
         this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
+        this.transactions = new ArrayList<>();
+        this.uniqueID = UUID.randomUUID().toString();
     }
-
+    
     public void deposit(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException("amount must be greater than zero");
         } else {
-            transactions.add(new Transaction(amount));
+            transactions.add(new Transaction(amount,"Deposit"));
+            balance = balance + amount;
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        } else {
+            transactions.add(new Transaction(amount,"Withdrawal"));
+            balance = balance - amount;
+        }
     }
-}
-
-    public double interestEarned() {
-        double amount = sumTransactions();
+    
+    public double calculateInterest(){
         switch(accountType){
             case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
+                if (balance <= 1000){
+                    return balance += balance * 0.001;
+                }
+                else{
+                    return balance += 1 + (balance-1000) * 0.002;
+                }
             case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
+                if (balance <= 1000){
+                    return balance += balance * 0.02;
+                }
+                if (balance <= 2000){
+                    return balance += 20 + (balance-1000) * 0.05;
+                }
+                return balance += 70 + (balance-2000) * 0.1;
             default:
-                return amount * 0.001;
+                return balance += balance * 0.001;
         }
     }
 
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
-
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
-        return amount;
-    }
-
-    public int getAccountType() {
+    public AccountType getAccountType() {
         return accountType;
     }
 
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+    
+    
+    
+    
+    
+    
 }
+
