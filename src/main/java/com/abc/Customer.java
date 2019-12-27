75e1deb4350a32e519736670d09cc2a230bfa2ae
batuhan-ymz <59225358+batuhan-ymz@users.noreply.github.com>
2019-12-27 10:02:31 +0000

    Update Customer.java

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,78 +1,113 @@
-package com.abc;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static java.lang.Math.abs;
+import java.util.ArrayList;
 
-public class Customer {
-    private String name;
-    private List<Account> accounts;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author batuhan yilmaz
+ */
+
+public class Customer extends User{
+    private final ArrayList<Account> accounts;
+    
     public Customer(String name) {
-        this.name = name;
-        this.accounts = new ArrayList<Account>();
+        super(name);
+        accounts = new ArrayList<>();
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public Customer openAccount(Account account) {
+    
+    public Customer createAccount(Account account){
         accounts.add(account);
         return this;
     }
-
-    public int getNumberOfAccounts() {
-        return accounts.size();
+    
+    public void deposit(String uid,double amount) {
+        Account a = findAccount(uid);
+        a.deposit(amount);
     }
 
-    public double totalInterestEarned() {
-        double total = 0;
-        for (Account a : accounts)
-            total += a.interestEarned();
-        return total;
+    public void withdraw(String uid,double amount) {
+        Account a = findAccount(uid);
+        a.withdraw(amount);
     }
-
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
+    
+    public void transferFunds(String uidFrom,double amountDeposit, String uidTo){
+        Account a = findAccount(uidFrom);
+        Account b = findAccount(uidTo);
+        
+        a.withdraw(amountDeposit);
+        b.deposit(amountDeposit);
+    }
+    
+    public String getAllStatements(){
+        String s = "Statement for " + this.getName() + "\n\n";
+        int totalBalance = 0;
+        
+        for(Account a : accounts){
+            s += getStatement(a);
+            totalBalance += a.getBalance();
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
+        s += "TotalBalance $" + totalBalance;
+        return s;
     }
-
-    private String statementForAccount(Account a) {
+    
+    public String getStatement(Account account){
         String s = "";
-
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
+        
+        switch(account.getAccountType()){
+            case CHECKING:
                 s += "Checking Account\n";
                 break;
-            case Account.SAVINGS:
+            case SAVINGS:
                 s += "Savings Account\n";
                 break;
-            case Account.MAXI_SAVINGS:
+            case MAXI_SAVINGS:
                 s += "Maxi Savings Account\n";
                 break;
         }
-
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
+        
+        for(Transaction t : account.getTransactions()){
+            s += "" + t.getType();
+            s += " $" + t.getAmount()+ "\n" + "\n";
         }
-        s += "Total " + toDollars(total);
+        s += "Total " + toDollars(account.getBalance()) + "\n" + "\n";
         return s;
     }
-
+    
+    public double totalInterestEarned() {
+        double total = 0;
+        for (Account a : accounts){
+            total += a.calculateInterest();
+        }
+        return total;
+    }
+    
     private String toDollars(double d){
         return String.format("$%,.2f", abs(d));
     }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+    
+    public Account findAccount(String uid){
+        boolean found = false;
+        int i = 0;
+        while(!found && i < accounts.size()){
+            if(accounts.get(i).getUniqueID().equals(uid)){
+                found = true;
+                return accounts.get(i);
+            }
+            else{
+                i++;
+            }
+        }
+    throw new IllegalArgumentException("Account doesnt exist");
+    }
+
 }
