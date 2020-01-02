e9bcdd6ef5a0bbf0dfb4b78b25d1d3547dcd08a1
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 15:59:23 +0000

    Add tests for Bank.totalInterestPaid()

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BankTest {
+    private static final double DOUBLE_DELTA = 1e-15;
 
     @Test
     public void oneCustomerWithZeroAccountsSummary() {
@@ -79,5 +80,75 @@ public class BankTest {
         bank.addCustomer(john);
 
         assertEquals("Customer Summary\n - Alice (2 accounts)\n - John (2 accounts)", bank.customerSummary());
+    }
+
+    @Test
+    public void interestPaidOnNewBankIs0() {
+        Bank bank = new Bank();
+        assertEquals(0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestPaidOnBankWith1NewCustomerIs0() {
+        Bank bank = new Bank();
+        bank.addCustomer(new Customer("Alice"));
+        assertEquals(0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestPaidOnBankWith1CustomerWith1NewAccountIs0() {
+        Bank bank = new Bank();
+        Customer alice = new Customer("Alice");
+        alice.openAccount(new Account(Account.SAVINGS));
+        bank.addCustomer(alice);
+        assertEquals(0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestPaidOnBankWith1CustomerWith1AccountIsEqualToInterestOnAccount() {
+        Bank bank = new Bank();
+        Customer alice = new Customer("Alice");
+        Account account = new Account(Account.SAVINGS);
+        alice.openAccount(account);
+        account.deposit(1000);
+        bank.addCustomer(alice);
+
+        double accountInterest = account.interestEarned();
+        assertEquals(accountInterest, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestPaidOnBankWith2CustomersWith2AccountsIsEqualToTotalInterestOnAccounts() {
+        Bank bank = new Bank();
+        double totalInterest = 0;
+
+        Customer alice = new Customer("Alice");
+        Account aliceAccount1 = new Account(Account.SAVINGS);
+        alice.openAccount(aliceAccount1);
+        aliceAccount1.deposit(5000);
+        totalInterest += aliceAccount1.interestEarned();
+
+        Account aliceAccount2 = new Account(Account.CHECKING);
+        alice.openAccount(aliceAccount2);
+        aliceAccount2.deposit(2000);
+        totalInterest += aliceAccount2.interestEarned();
+
+        bank.addCustomer(alice);
+
+        Customer bob = new Customer("Bob");
+        Account bobAccount1 = new Account(Account.SAVINGS);
+        alice.openAccount(bobAccount1);
+        bobAccount1.deposit(1000);
+        totalInterest += bobAccount1.interestEarned();
+
+        Account bobAccount2 = new Account(Account.MAXI_SAVINGS);
+        alice.openAccount(bobAccount2);
+        bobAccount2.deposit(5000);
+        totalInterest += bobAccount2.interestEarned();
+
+        bank.addCustomer(bob);
+
+        double accountInterest = totalInterest;
+        assertEquals(accountInterest, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 }
