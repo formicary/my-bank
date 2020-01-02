9f0c690f3518b89f559c44f4296e0919a30bfb08
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 15:24:03 +0000

    Test interest on maxi savings account

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -84,4 +84,31 @@ public class AccountTest {
         checkingAccount.deposit(1500);
         assertEquals(2, checkingAccount.interestEarned(), DOUBLE_DELTA);
     }
+
+    @Test
+    public void interestOnNewMaxiSavingsAccountIs0() {
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        assertEquals(0, checkingAccount.interestEarned(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestOnMaxiSavingsAccountWith1000Is20() {
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        checkingAccount.deposit(1000);
+        assertEquals(20, checkingAccount.interestEarned(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestOnMaxiSavingsAccountWith2000Is70() {
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        checkingAccount.deposit(2000);
+        assertEquals(70, checkingAccount.interestEarned(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void interestOnMaxiSavingsAccountWith2010Is71() {
+        Account checkingAccount = new Account(Account.MAXI_SAVINGS);
+        checkingAccount.deposit(2010);
+        assertEquals(71, checkingAccount.interestEarned(), DOUBLE_DELTA);
+    }
 }
