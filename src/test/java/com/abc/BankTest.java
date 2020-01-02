cdee387d8bbb784b441e9c89ce4fb623d1250ae5
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 15:34:05 +0000

    Add tests for bank.customerSummary()

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -8,7 +8,16 @@ public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
     @Test
-    public void customerSummary() {
+    public void oneCustomerWithZeroAccountsSummary() {
+        Bank bank = new Bank();
+        Customer john = new Customer("John");
+        bank.addCustomer(john);
+
+        assertEquals("Customer Summary\n - John (0 accounts)", bank.customerSummary());
+    }
+
+    @Test
+    public void oneCustomerWithOneAccountSummary() {
         Bank bank = new Bank();
         Customer john = new Customer("John");
         john.openAccount(new Account(Account.CHECKING));
@@ -17,4 +26,59 @@ public class BankTest {
         assertEquals("Customer Summary\n - John (1 account)", bank.customerSummary());
     }
 
+    @Test
+    public void oneCustomerWithTwoAccountsSummary() {
+        Bank bank = new Bank();
+        Customer alice = new Customer("Alice");
+        alice.openAccount(new Account(Account.CHECKING));
+        alice.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(alice);
+
+        assertEquals("Customer Summary\n - Alice (2 accounts)", bank.customerSummary());
+    }
+
+    @Test
+    public void twoCustomersWithOneAccountEachSummary() {
+        Bank bank = new Bank();
+        Customer alice = new Customer("Alice");
+        alice.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(alice);
+
+        Customer john = new Customer("John");
+        john.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(john);
+
+        assertEquals("Customer Summary\n - Alice (1 account)\n - John (1 account)", bank.customerSummary());
+    }
+
+    @Test
+    public void twoCustomersWithOneAccountAndTwoAccountsSummary() {
+        Bank bank = new Bank();
+        Customer alice = new Customer("Alice");
+        alice.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(alice);
+
+        Customer john = new Customer("John");
+        john.openAccount(new Account(Account.CHECKING));
+        john.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(john);
+
+        assertEquals("Customer Summary\n - Alice (1 account)\n - John (2 accounts)", bank.customerSummary());
+    }
+
+    @Test
+    public void twoCustomersWithTwoAccountsEachSummary() {
+        Bank bank = new Bank();
+        Customer alice = new Customer("Alice");
+        alice.openAccount(new Account(Account.CHECKING));
+        alice.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(alice);
+
+        Customer john = new Customer("John");
+        john.openAccount(new Account(Account.CHECKING));
+        john.openAccount(new Account(Account.CHECKING));
+        bank.addCustomer(john);
+
+        assertEquals("Customer Summary\n - Alice (2 accounts)\n - John (2 accounts)", bank.customerSummary());
+    }
 }
