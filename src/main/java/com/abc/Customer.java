bce0e4c697ab9d7c4c7a6d211e4a5783e9d5f7df
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 14:55:21 +0000

    Use StringBuilder when generating statements

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -35,41 +35,40 @@ public class Customer {
     }
 
     public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
+        StringBuilder statement = new StringBuilder("Statement for " + name + "\n");
         double total = 0.0;
         for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
+            statement.append("\n").append(statementForAccount(a)).append("\n");
             total += a.sumTransactions();
         }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
+        statement.append("\nTotal In All Accounts ").append(toDollars(total));
+        return statement.toString();
     }
 
     private String statementForAccount(Account a) {
-        String s = "";
+        StringBuilder s = new StringBuilder();
 
        //Translate to pretty account type
         switch(a.getAccountType()){
             case Account.CHECKING:
-                s += "Checking Account\n";
+                s.append("Checking Account\n");
                 break;
             case Account.SAVINGS:
-                s += "Savings Account\n";
+                s.append("Savings Account\n");
                 break;
             case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
+                s.append("Maxi Savings Account\n");
                 break;
         }
 
         //Now total up all the transactions
         double total = 0.0;
         for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
+            s.append("  ").append(t.amount < 0 ? "withdrawal" : "deposit").append(" ").append(toDollars(t.amount)).append("\n");
             total += t.amount;
         }
-        s += "Total " + toDollars(total);
-        return s;
+        s.append("Total ").append(toDollars(total));
+        return s.toString();
     }
 
     private String toDollars(double d){
