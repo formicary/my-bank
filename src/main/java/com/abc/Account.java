3bbc4c05eca7d28a047d899d8cd46d8df07da9b7
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 15:04:14 +0000

    Make Account.interestEarned() throw Exception on invalid account type

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -39,23 +39,25 @@ public class Account {
 
     public double interestEarned() {
         double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
+        switch (accountType) {
+            case CHECKING:
                 return amount * 0.001;
+            case SAVINGS:
+                if (amount <= 1000) {
+                    return amount * 0.001;
+                } else {
+                    return 1 + (amount-1000) * 0.002;
+                }
+            case MAXI_SAVINGS:
+                if (amount <= 1000) {
+                    return amount * 0.02;
+                } else if (amount <= 2000) {
+                    return 20 + (amount-1000) * 0.05;
+                } else {
+                    return 70 + (amount - 2000) * 0.1;
+                }
+            default:
+                throw new IllegalArgumentException("Invalid account type: " + accountType);
         }
     }
 
