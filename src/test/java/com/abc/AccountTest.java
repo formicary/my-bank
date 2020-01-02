53113c17d509f07f6bc492ccfbf75da0aedf8098
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 14:43:30 +0000

    Make test for depositing zero expect an Exception

--- src/test/java/com/abc/AccountTest.java
+++ src/test/java/com/abc/AccountTest.java
@@ -7,11 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class AccountTest {
     private static final double DOUBLE_DELTA = 1e-15;
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void zeroDeposit() {
         Account checkingAccount = new Account(Account.CHECKING);
         checkingAccount.deposit(0);
-        assertEquals(0, checkingAccount.sumTransactions(), DOUBLE_DELTA);
     }
 
     @Test
