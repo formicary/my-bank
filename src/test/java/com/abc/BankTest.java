dbb336006dd9045f236ede785fe02e6de063d92f
DanTm99 <danyaal99@hotmail.co.uk>
2020-01-02 15:34:24 +0000

    Remove DOUBLE_DELTA from BankTest

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class BankTest {
-    private static final double DOUBLE_DELTA = 1e-15;
 
     @Test
     public void oneCustomerWithZeroAccountsSummary() {
