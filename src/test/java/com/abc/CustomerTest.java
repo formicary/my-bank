2db4bc352b7ebf2473f66d5766fd13fc5004ee6b
Timothy Oluwole <to-kaminari@hotmail.com>
2020-01-10 16:55:49 +0000

    Update CustomerTest.java

--- src/test/java/com/abc/CustomerTest.java
+++ src/test/java/com/abc/CustomerTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class CustomerTest {
     private static final double DOUBLE_DELTA = 1e-15;
