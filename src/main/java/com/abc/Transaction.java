1612a7b01361510cde5d76bbb036fac85c418e7a
Timothy Oluwole <to-kaminari@hotmail.com>
2020-01-10 16:53:58 +0000

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,26 @@
 package com.abc;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class Transaction {
     public final double amount;
+    public final double newBalance;
 
     private Date transactionDate;
+    private DateProvider dateProvider;
 
-    public Transaction(double amount) {
+    public Transaction(double amount, double newBalance) {
         this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+        this.newBalance = newBalance;
+        this.dateProvider = new DateProvider();
+        this.transactionDate = dateProvider.now();
     }
-
+    
+    public DateProvider getDateProvider() {
+    	return dateProvider;
+    }
+    
+    public Date getTransactionDate() {
+    	return this.transactionDate;
+    }
 }
