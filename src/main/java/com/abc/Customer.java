89069319a4861d77dabd8d572edad6363ec4d47c
Rokas101 <rokius.bagdonas@gmail.com>
2020-01-12 11:38:30 +0000

    Customer.transferMoney method

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -46,5 +46,59 @@ public class Customer {
 
 
 
+    /**
+     * 0. validate that accounts are not null and belong to this Customer
+     * 1. validate if accountFrom has sufficient amount of money to be transfered
+     * 2. amount must be more than zero
+     * 3. 
+     * TODO: consider adding transfer statuses to be outputted for particular interface instead of exceptions
+     *  
+     * @param accountTo
+     * @param amount
+     */
+    public void transferMoney(Account accountFrom, Account accountTo, double amount) throws NullPointerException, IllegalArgumentException {
+        if(amount <= 0){
+            throw new IllegalArgumentException("Amount of money transfered must be more than 0. Current: " + amount);
+        }
+        if(accountFrom == null || accountTo == null){
+            throw new NullPointerException("Account money trasfered from or/and account transfered to does not exist");
+        }
+        if(accountFrom == accountTo){
+            throw new IllegalArgumentException("Cannot transfer money to the same account");
+        }
+
+        double accountFromDebit = accountFrom.sumTransactions();
+        if(accountFromDebit < amount){
+            throw new IllegalArgumentException(
+            "Account has has insufficient funds to perform the operation.\n" +
+            "Money in the account :" + 
+            Utilities.toDollars(accountFromDebit) + ". Amount attempted to transfer: " + Utilities.toDollars(amount));
+        }
+
+        //validate if accountFrom and accountTo belong to the same Customer
+        boolean belongToSameAccount = false;
+        boolean customersAccountFrom = false;
+        boolean customersAccountTo = false;
+
+        for(Account a : accounts){
+            if(a == accountFrom) customersAccountFrom = true;
+            if(a == accountTo) customersAccountTo = true;
+            if( customersAccountFrom && customersAccountTo ){
+                belongToSameAccount = true;
+                break;
+            }
+        }
+
+        if(!belongToSameAccount){
+            throw new IllegalArgumentException("Provided account(s) do not belong the same Customer");
+        }
+
+        //transfer the money
+        accountFrom.withdraw(amount);
+        accountTo.deposit(amount);
+
+    }
+
+
 
 }
