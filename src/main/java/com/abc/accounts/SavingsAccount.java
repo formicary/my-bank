e7a0e37facba61f1732ca2bb74c4c35588ad4ade
Rokas101 <rokius.bagdonas@gmail.com>
2020-01-13 17:43:21 +0000

    SavingsAccount compound interest calculation

--- src/main/java/com/abc/accounts/SavingsAccount.java
+++ src/main/java/com/abc/accounts/SavingsAccount.java
@@ -19,12 +19,22 @@ public class SavingsAccount extends Account {
 
     public double interestEarned() {
         double amount = sumTransactions();
+        int daysElapsed = getTransactionPeriod();
+        if(daysElapsed < 1) daysElapsed = 1;
+        double transactionSum = sumTransactions();
+
         if(amount >= Double.MAX_VALUE)
         if (amount <= moneyCap1){
-            return amount * interestRate1;
+            return calculateInterest1(transactionSum, daysElapsed);
         }
-        return (amount - moneyCap1) * interestRate2 + moneyCap1 * interestRate1 ;
+        return (amount - moneyCap1) * Math.pow(interestRate2/dateProvider.getYearDays(), daysElapsed)
+                 + calculateInterest1(moneyCap1, daysElapsed);
     }
+
+    private double calculateInterest1(double transactionSum, int daysElapsed){
+        return transactionSum * Math.pow(interestRate1/dateProvider.getYearDays(), daysElapsed);
+    }
+
 
     public String toString(){
         return "Savings Account";
