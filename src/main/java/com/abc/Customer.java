24772bcd49815542a0a647f005320f257143afa3
Nikolett Liesinger <nikolett.liesinger@accenture.com>
2022-08-26 17:20:39 +0200

    create CustomerStatement, move statement related methods from Customer

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -1,9 +1,8 @@
 package com.abc;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-import static java.lang.Math.abs;
 
 public class Customer {
     private final String name;
@@ -12,10 +11,6 @@ public class Customer {
     public Customer(String name) {
         this.name = name;
         this.accounts = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
     }
 
     public Customer openAccount(Account account) {
@@ -34,50 +29,15 @@ public class Customer {
         return total;
     }
 
-    private double sumAllTransactions() {
-        double total = 0.0;
-        for (Account account :
-                accounts) {
-            total += account.sumTransactions();
-        }
-        return total;
-    }
-
     public String getStatement() {
-        final String lineBreak = "\n";
-        StringBuilder statement = new StringBuilder("Statement for ");
-        statement.append(name);
-        statement.append(lineBreak);
-        for (Account account : accounts) {
-            statement.append(lineBreak);
-            statement.append(statementForAccount(account));
-            statement.append(lineBreak);
-        }
-        statement.append(lineBreak);
-        statement.append("Total In All Accounts ");
-        statement.append(toDollars(sumAllTransactions()));
-        return statement.toString();
+        return CustomerStatement.createStatement(this);
     }
 
-    private String statementForAccount(Account account) {
-        final String lineBreak = "\n";
-        StringBuilder sb = new StringBuilder(account.getAccountType().getPrettyName());
-        sb.append(lineBreak);
-        for (Transaction t : account.transactions) {
-            if (t.getAmount() < 0) {
-                sb.append("  withdrawal ");
-            } else {
-                sb.append("  deposit ");
-            }
-            sb.append(toDollars(t.getAmount()));
-            sb.append(lineBreak);
-        }
-        sb.append("Total ");
-        sb.append(toDollars(account.sumTransactions()));
-        return sb.toString();
+    public String getName() {
+        return name;
     }
 
-    private String toDollars(double d) {
-        return String.format("$%,.2f", abs(d));
+    public List<Account> getAccounts() {
+        return Collections.unmodifiableList(accounts);
     }
 }
